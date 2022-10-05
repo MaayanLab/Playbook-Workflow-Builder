a011d4dfa01f1eee60f332c23ab5ce4d20d94559
Daniel Clarke <u8sand@gmail.com>
2022-10-05 11:34:55 -0400

    Some type corrections in app/nodes

--- app/nodes.ts
+++ app/nodes.ts
@@ -1,9 +1,9 @@
 import * as components from '@/components'
-import { MetaNode, MetaNodeAny, MetaNodeData, MetaNodeProcessPrompt, MetaNodeProcessRun } from '@/spec/metanode'
+import { MetaNode, MetaNodeAny, MetaNodeData, MetaNodeProcessPrompt, MetaNodeProcessResolve } from '@/spec/metanode'
 
 const nodes = Object.keys(components)
   .map(component_name => components[component_name])
   .filter((component): component is MetaNode<MetaNodeAny> => component instanceof MetaNode)
 
 export const dataNodes = nodes.reduce<Record<string, MetaNode<MetaNodeData>>>((N, component) => component.t.kind === 'data' ? {...N, [component.t.spec]: component as any} : N, {})
-export const processNodes = nodes.reduce<Record<string, MetaNode<MetaNodeProcessPrompt | MetaNodeProcessRun>>>((N, component) => component.t.kind === 'process' ? {...N, [component.t.spec]: component as any} : N, {})
+export const processNodes = nodes.reduce<Record<string, MetaNode<MetaNodeProcessPrompt | MetaNodeProcessResolve>>>((N, component) => component.t.kind === 'process' ? {...N, [component.t.spec]: component as any} : N, {})

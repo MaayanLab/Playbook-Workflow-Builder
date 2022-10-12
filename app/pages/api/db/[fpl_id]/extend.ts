1c2897960219358e393a722eed484134ad49dabc
Daniel Clarke <u8sand@gmail.com>
2022-10-12 13:22:46 -0400

    Fixup db API impl

--- app/pages/api/db/[fpl_id]/extend.ts
+++ app/pages/api/db/[fpl_id]/extend.ts
@@ -19,7 +19,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (old_fpl === undefined) {
       res.status(404).end()
     } else {
-      const fpl = old_fpl.extend(process)
+      const fpl = fpprg.upsertFPL(old_fpl.extend(process))
       res.status(200).json(fpl.id)
     }
   } catch (e) {
--- app/pages/api/db/extend.ts
+++ app/pages/api/db/extend.ts
@@ -8,7 +8,6 @@ const BodyType = IdOrProcessC
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method !== 'POST') throw new Error('Unsupported method')
-    console.log(req.body)
     const process = fpprg.resolveProcess(BodyType.parse(JSON.parse(req.body)))
     const fpl = fpprg.upsertFPL(new FPL(process))
     res.status(200).json(fpl.id)

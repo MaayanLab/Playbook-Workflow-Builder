b7da41a8ee9c2148630b8b9b6a486739ac5bfeb3
Daniel J. B. Clarke <u8sand@gmail.com>
2022-10-25 17:22:08 -0400

    Update prebuild to ignore python artifacts

--- cli/sync-component-import.ts
+++ cli/sync-component-import.ts
@@ -1,9 +1,13 @@
 import fs from 'fs'
 import path from 'path'
+
+const components = fs.readdirSync(path.join(__dirname, '..', 'components'))
+  .filter(p => !p.startsWith('_') && fs.lstatSync(path.join(__dirname, '..', 'components', p)).isDirectory())
+  .map(p => path.basename(p))
+components.sort()
+
 fs.writeFileSync('components/index.ts',
-  fs.readdirSync(path.join(__dirname, '..', 'components'))
-    .map(p => path.basename(p))
-    .filter(component => component !== 'index.ts')
+  components
     .map(component => `export * from ${JSON.stringify('./' + path.basename(component))}`)
     .join('\n')
 )

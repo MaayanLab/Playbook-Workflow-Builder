560bfdc92f93699d83e264d6fe3198d69c26f74a
Daniel Clarke <u8sand@gmail.com>
2022-10-28 13:57:45 -0400

    Dump kvdb in suggest GET

--- app/pages/api/suggest.tsx
+++ app/pages/api/suggest.tsx
@@ -18,39 +18,44 @@ const BodyType = z.object({
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    if (req.method !== 'POST') throw new Error('Unsupported method')
-    const suggestion = BodyType.parse(JSON.parse(req.body))
-    // add the suggested KRG node(s)
-    let OutputNode = krg.getDataNode(suggestion.output)
-    if (OutputNode === undefined) {
-      OutputNode = MetaNode.createData(suggestion.output)
+    if (req.method === 'GET')  {
+      res.status(200).json(kvdb)
+    } else if (req.method === 'POST') {
+      const suggestion = BodyType.parse(JSON.parse(req.body))
+      // add the suggested KRG node(s)
+      let OutputNode = krg.getDataNode(suggestion.output)
+      if (OutputNode === undefined) {
+        OutputNode = MetaNode.createData(suggestion.output)
+          .meta({
+            label: suggestion.output,
+            description: `A data type, suggested as part of ${suggestion.name}`,
+          })
+          .codec<any>()
+          .view((props) => {
+            return <div>This data type was suggested as part of {suggestion.name}</div>
+          })
+          .build()
+        krg.add(OutputNode)
+      }
+      const ProcessNode = MetaNode.createProcess(suggestion.name)
         .meta({
-          label: suggestion.output,
-          description: `A data type, suggested as part of ${suggestion.name}`,
+          label: suggestion.name,
+          description: suggestion.description,
         })
-        .codec<any>()
-        .view((props) => {
-          return <div>This data type was suggested as part of {suggestion.name}</div>
+        .inputs(dict.init(suggestion.inputs.split(',').filter(s => s != '').map((spec, ind) =>
+        ({ key: ind.toString(), value: krg.getDataNode(spec) }))))
+        .output(OutputNode)
+        .prompt((props) => {
+          return <div>This was suggested by {suggestion.author_name} &lt;{suggestion.author_email}&gt; ({suggestion.author_org})</div>
         })
         .build()
-      krg.add(OutputNode)
+      krg.add(ProcessNode)
+      // register the suggestion
+      kvdb[uuidv4()] = JSON.stringify(suggestion)
+      res.status(200).end()
+    } else {
+      throw new Error('Unsupported method')
     }
-    const ProcessNode = MetaNode.createProcess(suggestion.name)
-      .meta({
-        label: suggestion.name,
-        description: suggestion.description,
-      })
-      .inputs(dict.init(suggestion.inputs.split(',').filter(s => s != '').map((spec, ind) =>
-      ({ key: ind.toString(), value: krg.getDataNode(spec) }))))
-      .output(OutputNode)
-      .prompt((props) => {
-        return <div>This was suggested by {suggestion.author_name} &lt;{suggestion.author_email}&gt; ({suggestion.author_org})</div>
-      })
-      .build()
-    krg.add(ProcessNode)
-    // register the suggestion
-    kvdb[uuidv4()] = JSON.stringify(suggestion)
-    res.status(200).end()
   } catch (e) {
     console.error(e)
     res.status(500).end((e as Error).toString())

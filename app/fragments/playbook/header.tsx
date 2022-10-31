d587ae7c7a7a60aaeb78ebec1b6d8dd742df0687
Daniel Clarke <u8sand@gmail.com>
2022-10-31 11:45:52 -0400

    Add links to the sub pages on the home page

--- app/fragments/playbook/header.tsx
+++ app/fragments/playbook/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <div className="bg-primary">
-      <Link href="/">
+      <Link href="/graph">
         <h1 className="text-4xl font-bold p-2 cursor-pointer">Playbook Partnership Interactive Workflow Builder</h1>
       </Link>
     </div>

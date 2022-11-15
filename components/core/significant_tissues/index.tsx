981a068dc3359dd589964edfc78e9d1d4970aa8a
Daniel Clarke <u8sand@gmail.com>
2022-11-15 14:48:56 -0500

    [significant_tissues]: Use blueprintjs table rendering

--- components/core/significant_tissues/index.tsx
+++ components/core/significant_tissues/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { MetaNode } from '@/spec/metanode'
 import { z } from 'zod'
 import { tissue_icon, weighted_icon } from '@/icons'
+import { Table2 as Table, Column, Cell } from '@blueprintjs/table'
 
 export const SignificantTissueC = z.array(z.object({
   tissue: z.string().describe('The tissue term'),
@@ -15,22 +16,18 @@ export const SignificantTissues = MetaNode.createData('SignificantTissues')
     icon: [weighted_icon, tissue_icon],
   })
   .codec(SignificantTissueC)
-  .view(tissues => (
-    <table>
-      <thead>
-        <tr>
-          <th>Tissue</th>
-          <th>ZScore</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tissues.map(tissue =>
-          <tr key={tissue.tissue}>
-            <td>{tissue.tissue}</td>
-            <td>{tissue.zscore.toPrecision(3)}</td>
-          </tr>
-        )}
-      </tbody>
-    </table>
-  ))
+  .view(tissues => {
+    return (
+      <Table numRows={tissues.length}>
+        <Column
+          name="Tissue"
+          cellRenderer={row => <Cell>{tissues[row].tissue}</Cell>}
+        />
+        <Column
+          name="ZScore"
+          cellRenderer={row => <Cell>{tissues[row].zscore.toPrecision(3)}</Cell>}
+        />
+      </Table>
+    )
+  })
   .build()

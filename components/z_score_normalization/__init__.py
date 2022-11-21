7002907f15b5daa706483af9b474d9ee2325e037
Daniel Clarke <u8sand@gmail.com>
2022-11-21 17:50:43 -0500

    Filter out NaNs resulting from z score norm

--- components/z_score_normalization/__init__.py
+++ components/z_score_normalization/__init__.py
@@ -1,3 +1,4 @@
+import numpy as np
 from scipy.stats import zscore
 from components.gene_count_matrix import gene_count_matrix, anndata_from_path
 from components.file import upsert_file
@@ -8,6 +9,9 @@ def z_score_normalize_gene_count_matrix(m):
   # z-score normalization
   df.X = zscore(df.X, axis=1)
 
+  # filter out genes with any null variances
+  df = df[~np.isnan(df.X).any(axis=1), :]
+ 
   with upsert_file('.h5ad') as f:
     df.write_h5ad(f.file)
 

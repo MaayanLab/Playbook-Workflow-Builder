0698c45969b7e814b9f6c25154a290169fc22199
Daniel Clarke <u8sand@gmail.com>
2022-12-02 18:38:47 -0500

    Fixup FPPRG FPL API strict type

--- app/pages/api/db/[fpl_id]/rebase/[process_id].ts
+++ app/pages/api/db/[fpl_id]/rebase/[process_id].ts
@@ -23,8 +23,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       if (old_fpl === undefined) {
         res.status(404).end()
       } else {
-        const fpl = fpprg.upsertFPL(old_fpl.rebase(old_process, new_process))
-        res.status(200).json(fpl.id)
+        const new_fpl = old_fpl.rebase(old_process, new_process)
+        if (new_fpl === undefined) {
+          res.status(404).end()
+        } else {
+          const fpl = fpprg.upsertFPL(new_fpl)
+          res.status(200).json(fpl.id)
+        }
       }
     }
   } catch (e) {

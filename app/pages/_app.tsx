71d4ff6dd4ed86144c5a7057e65b28156f69efbc
Daniel Clarke <u8sand@gmail.com>
2022-12-02 18:12:50 -0500

    Fixup app type

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
 import '@/app/styles/styles.css'
-export default function App({ Component, pageProps }) {
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }

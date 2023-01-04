8646cede274e50dfb43feaa433e002c72ec50e66
Daniel Clarke <u8sand@gmail.com>
2023-01-04 18:13:38 -0500

    WIP: pg orm

--- spec/sql.ts
+++ spec/sql.ts
@@ -6,6 +6,10 @@ import { z } from "zod"
 import * as dict from '@/utils/dict'
 
 export type TypedSchema<T = {}> = z.ZodType<T> & {
+  name: string
+  field_types: {[K in keyof T]: z.ZodType<T[K]>}
+  field_sql: {[K in keyof T]: string}
+  extra_insert_sql?: string,
   schema_up: string
   schema_down: string
 }
@@ -14,7 +18,9 @@ export type TableSchema<T = {}> = {
   name: string
   field_types: {[K in keyof T]: z.ZodType<T[K]>}
   field_sql: {[K in keyof T]: string}
+  field_extra_sql: {[K in keyof T]: string}
   extra_sql: string[]
+  extra_insert_sql: string[],
 }
 
 /**
@@ -27,28 +33,36 @@ export type TableSchema<T = {}> = {
 export class Table<T = {}> {
   constructor(public t: TableSchema<T>) {}
   static create(name: string) {
-    return new Table({ name, field_types: {}, field_sql: {}, extra_sql: [] })
+    return new Table({ name, field_types: {}, field_sql: {}, field_extra_sql: {}, extra_sql: [], extra_insert_sql: [] })
   }
-  field<S extends string, C = undefined>(name: S, sql: string, type: z.ZodType<C>) {
+  field<S extends string, C = undefined>(name: S, sql: string, extra_sql: string, type: z.ZodType<C>) {
     return new Table({
       ...this.t,
       field_types: { ...this.t.field_types, [name]: type },
       field_sql: { ...this.t.field_sql, [name]: sql },
+      field_extra_sql: { ...this.t.field_sql, [name]: extra_sql },
     } as TableSchema<T & { [K in S]: C }>)
   }
-  extra(sql: string) {
+  extra(extra_sql: string) {
     return new Table({
       ...this.t,
-      extra_sql: [...this.t.extra_sql, sql],
+      extra_sql: [...this.t.extra_sql, extra_sql],
+    })
+  }
+  extra_insert(extra_insert_sql: string) {
+    return new Table({
+      ...this.t,
+      extra_insert_sql: [...this.t.extra_insert_sql, extra_insert_sql],
     })
   }
   build() {
+    const { name, field_types, extra_insert_sql } = this.t
     const type = z.object(this.t.field_types)
     const schema_up = [
       `create table ${this.t.name} (`,
       [
-        ...dict.items(this.t.field_sql).map(
-          ({ key, value: sql }) => `  ${JSON.stringify(key)} ${sql}`
+        ...dict.keys(this.t.field_sql).map(field =>
+          `  ${JSON.stringify(field)} ${this.t.field_sql[field]} ${this.t.field_extra_sql[field]}`
         ),
         ...this.t.extra_sql.map(sql => `  ${sql}`),
       ].join(',\n'),
@@ -56,7 +70,7 @@ export class Table<T = {}> {
     ].join('\n')
     
     const schema_down = `drop table ${this.t.name};`
-    return { ...type, schema_up, schema_down } as unknown as TypedSchema<T>
+    return { ...type, name, field_types, extra_insert_sql, schema_up, schema_down } as unknown as TypedSchema<T>
   }
 }
 
@@ -81,9 +95,10 @@ export class View<T extends {} = {}> {
     return new View({ ...this.t, sql: sql.join('\n'), })
   }
   build() {
+    const { name, field_types } = this.t
     const type = z.object(this.t.field_types)
     const schema_up = `create view ${this.t.name} as ${this.t.sql}`
     const schema_down = `drop view ${this.t.name};`
-    return { ...type, schema_up, schema_down } as unknown as TypedSchema<T>
+    return { ...type, name, field_types, schema_up, schema_down } as unknown as TypedSchema<T>
   }
 }

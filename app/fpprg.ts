d916e171a700dae68610a3d684a1f68b7dd2c2f5
Daniel Clarke <u8sand@gmail.com>
2023-01-05 12:41:33 -0500

    Use PgDatabase when DATABASE_URL is specified

--- app/fpprg.ts
+++ app/fpprg.ts
@@ -1,5 +1,5 @@
 import krg from '@/app/krg'
-import { MemoryDatabase, Database } from '@/core/FPPRG'
+import { PgDatabase, MemoryDatabase, Database } from '@/core/FPPRG'
 import attach from '@/core/engine'
 declare global {
   var fpprg: Database | undefined
@@ -7,13 +7,12 @@ declare global {
 }
 
 /**
- * This trick persists the in-memory database in
- *  a global object across hot-reloads
+ * This trick persists the database in a global object across hot-reloads
  * 
  * If the global objects exist, we'll detach the old engine
  *  and attach the new one to the persistent fpprg
  */
-const fpprg = global.fpprg || new MemoryDatabase()
+const fpprg = global.fpprg || (process.env.DATABASE_URL ? new PgDatabase(process.env.DATABASE_URL) : new MemoryDatabase())
 if (global.fpprg && global.detach) global.detach()
 const detach = attach(krg, fpprg)
 global.fpprg = fpprg

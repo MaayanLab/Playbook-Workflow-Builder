b17dbe2fbb5124afe52c261a101c5e0f327cca98
Daniel Clarke <u8sand@gmail.com>
2023-01-06 15:28:25 -0500

    Rearrange engine a bit to decouple it

--- app/fpprg.ts
+++ app/fpprg.ts
@@ -1,6 +1,6 @@
 import krg from '@/app/krg'
 import { PgDatabase, MemoryDatabase, Database } from '@/core/FPPRG'
-import attach from '@/core/engine'
+import { process_insertion_dispatch } from '@/core/engine'
 declare global {
   var fpprg: Database | undefined
   var detach: (() => void) | undefined
@@ -14,7 +14,7 @@ declare global {
  */
 const fpprg = global.fpprg || (process.env.DATABASE_URL ? new PgDatabase(process.env.DATABASE_URL) : new MemoryDatabase())
 if (global.fpprg && global.detach) global.detach()
-const detach = attach(krg, fpprg)
+const detach = process_insertion_dispatch(krg, fpprg)
 global.fpprg = fpprg
 global.detach = detach
 export default fpprg
--- core/engine.ts
+++ core/engine.ts
@@ -5,54 +5,55 @@ import * as dict from '@/utils/dict'
 class UnboundError extends Error {}
 
 /**
+ * Given an instanceProcess, "resolve" its output, this is typically done
+ *  by executing it, though prompts are implicitly identity functions.
+ */
+export async function resolve_process(krg: KRG, instanceProcess: Process) {
+  try {
+    const metaProcess = krg.getProcessNode(instanceProcess.type)
+    if (metaProcess === undefined) throw new Error('Unrecognized process')
+    const props = {
+      data: instanceProcess.data,
+      inputs: dict.init(
+        dict.items(await instanceProcess.inputs__outputs()).map(({ key, value }) => {
+          if (value === undefined) {
+            // handle nodes
+            throw new UnboundError()
+          } else if (value.type === 'Error') {
+            // propagate errors
+            throw new Error(`${instanceProcess.type} can't run because of error in ${metaProcess.inputs[key as string].spec}`)
+          }
+          return { key, value: value ? metaProcess.inputs[key as string].codec.decode(value.value) : undefined }
+      })),
+    }
+    if ('prompt' in metaProcess) {
+      console.debug(`Output comes from data`)
+      return new Resolved(instanceProcess, instanceProcess.data)
+    } else {
+      const output = metaProcess.output.codec.encode(await metaProcess.resolve(props))
+      console.debug(`Calling action ${JSON.stringify(metaProcess.spec)} with props ${JSON.stringify(props)} of type ${JSON.stringify(metaProcess.inputs)} to produce ${JSON.stringify(metaProcess.output.spec)}: ${output}`)
+      return new Resolved(instanceProcess, new Data(metaProcess.output.spec, output))
+    }
+  } catch (e) {
+    if (e instanceof UnboundError) {
+      return new Resolved(instanceProcess, undefined)
+    } else {
+      console.error(e)
+      return new Resolved(instanceProcess, new Data('Error', JSON.stringify((e as Error).toString())))
+    }
+  }
+}
+
+/**
  * This is a minimally viable scg engine -- given a database,
  *  we'll reconcile process outputs when they are created.
- * 
- * Alternatively we could reconcile process outputs whenever they are
- *  requested from the db but aren't already present, this might be more ideal
- *  but might be tricker in a worker served environment.
- * 
- * Thus the way one uses SCG is by adding process nodes to the instance graph
- *  and awaiting the output.
- * 
  */
-export default function create_engine(krg: KRG, db: Database) {
+export function process_insertion_dispatch(krg: KRG, db: Database) {
   return db.listen(async (table, record) => {
     if (table === 'process') {
       const instanceProcess = record as Process
-      try {
-        const metaProcess = krg.getProcessNode(instanceProcess.type)
-        if (metaProcess === undefined) throw new Error('Unrecognized process')
-        const props = {
-          data: instanceProcess.data,
-          inputs: dict.init(
-            dict.items(await instanceProcess.inputs__outputs()).map(({ key, value }) => {
-              if (value === undefined) {
-                // handle nodes
-                throw new UnboundError()
-              } else if (value.type === 'Error') {
-                // propagate errors
-                throw new Error(`${instanceProcess.type} can't run because of error in ${metaProcess.inputs[key as string].spec}`)
-              }
-              return { key, value: value ? metaProcess.inputs[key as string].codec.decode(value.value) : undefined }
-          })),
-        }
-        if ('prompt' in metaProcess) {
-          console.debug(`Output comes from data`)
-          await db.upsertResolved(new Resolved(instanceProcess, instanceProcess.data))
-        } else {
-          const output = metaProcess.output.codec.encode(await metaProcess.resolve(props))
-          console.debug(`Calling action ${JSON.stringify(metaProcess.spec)} with props ${JSON.stringify(props)} of type ${JSON.stringify(metaProcess.inputs)} to produce ${JSON.stringify(metaProcess.output.spec)}: ${output}`)
-          await db.upsertResolved(new Resolved(instanceProcess, new Data(metaProcess.output.spec, output)))
-        }
-      } catch (e) {
-        if (e instanceof UnboundError) {
-          await db.upsertResolved(new Resolved(instanceProcess, undefined))
-        } else {
-          console.error(e)
-          await db.upsertResolved(new Resolved(instanceProcess, new Data('Error', JSON.stringify((e as Error).toString()))))
-        }
-      }
+      const resolved = await resolve_process(krg, instanceProcess)
+      await db.upsertResolved(resolved)
     }
   })
 }

62ed0ae2b6fc0dba6bdf3bc817a7b90682567c35
Daniel Clarke <u8sand@gmail.com>
2023-01-12 17:24:03 -0500

    Improve sitemap styling

--- app/pages/sitemap.tsx
+++ app/pages/sitemap.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
-import { Card, Elevation } from "@blueprintjs/core";
 import Link from 'next/link';
  
 const Header = dynamic(() => import('@/app/fragments/playbook/header'))
@@ -15,31 +14,43 @@ export default function SiteMap() {
         <title>Playbook</title>
       </Head>
       <Header homepage="/" />
-        <main className="flex-grow container mx-auto py-4 flex flex-col">
-          <h2 className="bp4-heading">Site Map</h2>
-          <p className="bp4-text-large">Click the card to go to the relevant page.</p>
-          <div className="grid md:grid-cols-3 gap-4 my-2">
-            <Link href="/graph">
-              <Card className="col-span-1" interactive={true} elevation={Elevation.TWO}>
-                <h5 className="bp4-heading">Graph Mode</h5>
-                <p>A graph interface for incrementally building complex workflows of MetaNodes.</p>
-              </Card>
-            </Link>
-            <Link href="/report">
-              <Card className="col-span-1" interactive={true} elevation={Elevation.TWO}>
-                <h5 className="bp4-heading">Report Mode</h5>
-                <p>A jupyter-notebook style interface for sequential execution of MetaNode Processes.</p>
-              </Card>
-            </Link>
-            <Link href="/testing">
-              <Card className="col-span-1" interactive={true} elevation={Elevation.TWO}>
-                <h5 className="bp4-heading">Prototyping Interface</h5>
-                <p>A simple interface for developing new components & inspecting internals.</p>
-              </Card>
-            </Link>
+      <main className="flex-grow container mx-auto py-4 flex flex-col">
+        <div className="hero">
+          <div className="hero-content prose">
+            <div>
+              <h2>Site Map</h2>
+              <p>Click the card to go to the relevant page.</p>
+              <div className="grid md:grid-cols-3 gap-4 my-2">
+                <Link href="/graph">
+                  <div className="card card-compact card-bordered col-span-1 bg-base-100 shadow shadow-slate-300 hover:shadow-slate-600 hover:cursor-pointer">
+                    <div className="card-body prose">
+                      <h4 className="card-title">Graph Mode</h4>
+                      <p>A graph interface for incrementally building complex workflows of MetaNodes.</p>
+                    </div>
+                  </div>
+                </Link>
+                <Link href="/report">
+                  <div className="card card-compact card-bordered col-span-1 bg-base-100 shadow shadow-slate-300 hover:shadow-slate-600 hover:cursor-pointer">
+                    <div className="card-body prose">
+                      <h4 className="card-title">Report Mode</h4>
+                      <p>A jupyter-notebook style interface for sequential execution of MetaNode Processes.</p>
+                    </div>
+                  </div>
+                </Link>
+                <Link href="/testing">
+                  <div className="card card-compact card-bordered col-span-1 bg-base-100 shadow shadow-slate-300 hover:shadow-slate-600 hover:cursor-pointer">
+                    <div className="card-body prose">
+                      <h4 className="card-title">Prototyping Interface</h4>
+                      <p>A simple interface for developing new components & inspecting internals.</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </div>
           </div>
-          <Contributors />
-        </main>
+        </div>
+        <Contributors />
+      </main>
       <Footer />
     </div>
   )

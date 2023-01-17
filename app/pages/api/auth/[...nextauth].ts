cc1134fb68dd87507e518e033f568ec071795285
Daniel Clarke <u8sand@gmail.com>
2023-01-17 17:50:57 -0500

    Expose user.id on session

--- app/pages/api/auth/[...nextauth].ts
+++ app/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,12 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions, Session } from 'next-auth'
 import EmailProvider from 'next-auth/providers/email'
 import MemoryAdapter from '@/app/extensions/next-auth/memory-adapter'
 import PgAdapter from '@/app/extensions/next-auth/pg-adapter'
 
-export default NextAuth({
+export type AuthOptions = typeof authOptions
+export type SessionWithId = Session & { user: { id: string } }
+
+export const authOptions: NextAuthOptions = {
   adapter: process.env.DATABASE_URL ? PgAdapter(process.env.DATABASE_URL) : MemoryAdapter(),
   providers: [
     EmailProvider({
@@ -11,4 +14,15 @@ export default NextAuth({
       from: process.env.EMAIL_FROM,
     })
   ],
-})
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) Object.assign(session.user, { id: token.sub })
+      return session as SessionWithId
+    }
+  },
+  session: {
+    strategy: 'jwt',
+  },
+}
+
+export default NextAuth(authOptions)

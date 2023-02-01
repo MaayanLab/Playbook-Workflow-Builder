7a61e850fb257ae7c62fda8a096473257c31eebc
Daniel Clarke <u8sand@gmail.com>
2023-02-01 10:22:24 -0500

    Use global cache helper for fpprg & krg

--- app/fpprg.ts
+++ app/fpprg.ts
@@ -2,22 +2,10 @@ import krg from '@/app/krg'
 import FPPRG from '@/core/FPPRG'
 import db from '@/app/db'
 import { start_workers } from '@/core/engine'
-declare global {
-  var fpprg: FPPRG | undefined
-}
+import cache from '@/utils/global_cache'
 
-/**
- * This trick persists the database in a global object across hot-reloads
- * 
- * If the global objects exist, we'll detach the old engine
- *  and attach the new one to the persistent fpprg
- */
-let fpprg: FPPRG
-if (!global.fpprg) {
-  global.fpprg = fpprg = new FPPRG(db)
+export default cache('fpprg', () => {
+  const fpprg = new FPPRG(db)
   start_workers(krg, fpprg, +(process.env.N_WORKERS||'1'))
-} else {
-  fpprg = global.fpprg
-}
-
-export default fpprg
+  return fpprg
+})
--- app/krg.ts
+++ app/krg.ts
@@ -2,18 +2,15 @@ import KRG from '@/core/KRG'
 import * as components from '@/components'
 import { ensureArray } from '@/utils/array'
 import { MetaNodeType } from '@/spec/metanode'
-
-declare global {
-  var krg: KRG | undefined
-}
+import cache from '@/utils/global_cache'
 
 type ValuesOf<T> = T[keyof T]
 
-const krg = global.krg || new KRG()
+const krg = cache('krg', () => new KRG())
+
 Object.values(components)
   .flatMap(component => ensureArray<ValuesOf<typeof components>>(component))
   .filter((component): component is MetaNodeType<unknown> => typeof component === 'object' && 'spec' in component)
   .forEach(metanode => krg.add(metanode))
 
-global.krg = krg
 export default krg

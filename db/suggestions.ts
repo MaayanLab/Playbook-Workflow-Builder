66f90deb1853d5e1e881d85ed48702fc231897ca
Daniel Clarke <u8sand@gmail.com>
2023-02-01 16:18:10 -0500

    Add uuid default to suggestions

--- db/suggestions.ts
+++ db/suggestions.ts
@@ -1,10 +1,11 @@
 import { Table } from '@/spec/sql'
 import { z } from 'zod'
+import { v4 as uuidv4 } from 'uuid'
 
 const z_uuid = z.string
 
 export const suggestion = Table.create('suggestion')
-  .field('id', 'uuid', 'default uuid_generate_v4()', z_uuid(), { primaryKey: true })
+  .field('id', 'uuid', 'default uuid_generate_v4()', z_uuid(), { primaryKey: true, default: uuidv4 })
   .field('name', 'varchar', 'not null', z.string())
   .field('inputs', 'varchar', 'not null', z.string())
   .field('output', 'varchar', 'not null', z.string())

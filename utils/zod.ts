e356575081ac6afde22a3e0d87ed9711b923895b
Daniel Clarke <u8sand@gmail.com>
2023-02-06 16:06:34 -0500

    Fixup suggestion datetime handling

--- utils/zod.ts
+++ utils/zod.ts
@@ -5,7 +5,7 @@ export const nullable_undefined_codec = <C>(type: z.ZodType<C>) => ({
   decode: type.nullable().transform(v => v !== null ? v : undefined).parse,
   encode: type.optional().transform(v => v !== undefined ? v : null).parse,
 })
-export const json_safe_timestamp_codec = <C>() => ({
-  decode: z.string().transform(v => new Date(v)).parse,
-  encode: z.date().transform(v => v.toISOString()).parse,
+export const json_safe_timestamp_codec = () => ({
+  decode: z.date().transform((v) => v.toISOString()).parse,
+  encode: z.string().transform((v) => new Date(v)).parse,
 })

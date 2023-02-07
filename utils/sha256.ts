3e2aed019608968e4ce87bbabb68ef0b9280588d
Daniel Clarke <u8sand@gmail.com>
2023-02-07 17:19:59 -0500

    Fix comment typo

--- utils/sha256.ts
+++ utils/sha256.ts
@@ -2,7 +2,7 @@ import { Hash } from "fast-sha256"
 import { canonicalize } from 'json-canonicalize'
 
 /**
- * Obtain a unique hash for a arbitrary data
+ * Obtain a unique hash for arbitrary data
  */
 export default function sha256(data: any) {
   const h = new Hash()

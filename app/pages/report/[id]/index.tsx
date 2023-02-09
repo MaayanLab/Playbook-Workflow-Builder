24a704bd84c4371d4f04c1f2ddc3b4a69f90b7b3
Daniel Clarke <u8sand@gmail.com>
2023-02-09 15:09:17 -0500

    Fixup stale tooltip on share button

--- app/pages/report/[id]/index.tsx
+++ app/pages/report/[id]/index.tsx
@@ -104,18 +104,21 @@ function ShareButton({ id }: { id: string | undefined }) {
     navigator.clipboard.writeText(graphUrl.value)
   }, [id])
   React.useEffect(() => { if (share) { onClick() } }, [share])
-  return (
-    <>
-      <button className={`bp4-button bp4-minimal${share ? ' hidden' : ''}`} onClick={() => {setShare(true)}}>
+  if (!share) {
+    return (
+      <button className={`bp4-button bp4-minimal`} onClick={() => {setShare(true)}}>
         <Icon icon={share_icon} color="black" />
       </button>
+    )
+  } else {
+    return (
       <div className={`bp4-control-group inline-block${share ? '': ' hidden'}`}>
         <input id="graph-url" type="text" className="bp4-input" value={`${publicUrl}/report${id ? `/${id}` : ''}`} readOnly />
         <button className="bp4-button bp4-icon-link" onClick={onClick} />
         <button className="bp4-button bp4-icon-cross" onClick={() => {setShare(false)}} />
       </div>
-    </>
-  )
+    )
+  }
 }
 
 function Cells({ krg, id }: { krg: KRG, id?: string }) {

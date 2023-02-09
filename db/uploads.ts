876646ca33732e69b782825100424ea89518b3b2
Daniel Clarke <u8sand@gmail.com>
2023-02-09 17:28:31 -0500

    [db] Expand user uploads with user_upload_complete view

--- db/uploads.ts
+++ db/uploads.ts
@@ -1,10 +1,11 @@
-import { Table } from '@/spec/sql'
+import { Table, View } from '@/spec/sql'
 import { z } from 'zod'
 import { v4 as uuidv4 } from 'uuid'
 import { z_uuid } from '@/utils/zod'
 
 export const upload = Table.create('upload')
-  .field('url', 'varchar', 'not null', z.string(), { primaryKey: true })
+  .field('id', 'uuid', 'default uuid_generate_v4()', z_uuid(), { primaryKey: true, default: uuidv4 })
+  .field('url', 'varchar', 'not null', z.string())
   .field('sha256', 'varchar', 'not null', z.string())
   .field('size', 'number', 'not null', z.number())
   .field('created', 'timestamp', 'not null default now()', z.date(), { default: () => new Date() })
@@ -16,4 +17,38 @@ export const user_upload = Table.create('user_upload')
   .field('upload', 'uuid', 'not null references "upload" ("id") on delete cascade', z_uuid())
   .field('filename', 'varchar', 'not null', z.string())
   .field('created', 'timestamp', 'not null default now()', z.date(), { default: () => new Date() })
+  .build()
+
+export const user_upload_complete = View.create('user_upload_complete')
+  .field('id', z_uuid(), { primaryKey: true })
+  .field('user', z_uuid())
+  .field('url', z.string())
+  .field('sha256', z.string())
+  .field('size', z.number())
+  .field('filename', z.string())
+  .field('created', z.string())
+  .sql(`
+    select
+      "user_upload"."id",
+      "user_upload"."user",
+      "upload"."url",
+      "upload"."sha256",
+      "upload"."size",
+      "user_upload"."filename",
+      "user_upload"."created"
+    from "user_upload"
+    left join "upload" on "user_upload"."upload" = "upload"."id"
+  `)
+  .js(async (db: any) => await Promise.all((await db.objects.user_upload.findMany()).map(async (user_upload: any) => {
+    const upload = await db.objects.upload.findUnique({ where: { id: user_upload.upload } })
+    return {
+      id: user_upload.id,
+      user: user_upload.user,
+      url: upload.url,
+      sha256: upload.sha256,
+      size: upload.size,
+      filename: user_upload.filename,
+      created: user_upload.created,
+    }
+  })))
   .build()

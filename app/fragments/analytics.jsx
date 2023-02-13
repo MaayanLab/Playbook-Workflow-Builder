ca098d95b26745c0a39cb075868213edbcb2aca9
Daniel Clarke <u8sand@gmail.com>
2023-02-13 16:11:45 -0500

    Add Matomo analytics alternative

--- app/fragments/analytics.jsx
+++ app/fragments/analytics.jsx
@@ -23,11 +23,35 @@ function GA({ id }) {
   />
 }
 
+function Matomo({ url, siteId }) {
+  const router = useRouter()
+  React.useEffect(() => {
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    _paq.push(['setTrackerUrl', url+'matomo.php']);
+    _paq.push(['setSiteId', siteId]);
+  }, [])
+  React.useEffect(() => {
+    if (typeof window._paq === 'undefined') return
+    window._paq.push(['setCustomUrl', router.asPath])
+    window._paq.push(['trackPageView'])
+  }, [router.asPath])
+  return <Script
+    src={`${url}/matomo.js`}
+    strategy="lazyOnload"
+  />
+}
+
 export default function Analytics() {
   return (
     <>
       {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ?
         <GA id={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
+        : null}
+      {process.env.NEXT_PUBLIC_MATOMO_URL && process.env.NEXT_PUBLIC_MATOMO_SITE_ID ?
+        <Matomo url={process.env.NEXT_PUBLIC_MATOMO_URL} siteId={process.env.NEXT_PUBLIC_MATOMO_SITE_ID} />
         : null}
     </>
   )

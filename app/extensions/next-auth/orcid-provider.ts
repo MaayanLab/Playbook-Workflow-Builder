45ec2b14ea8cd4a99c541054cbcb325d9c3cef5b
Daniel Clarke <u8sand@gmail.com>
2023-02-14 12:52:58 -0500

    Add ORCID OAuth provider

---
+++ app/extensions/next-auth/orcid-provider.ts
@@ -0,0 +1,29 @@
+import type { OAuthConfig, OAuthUserConfig } from "next-auth/providers"
+
+export default function ORCIDProvider<P extends { sub:string, name: string, email: string }>(options: OAuthUserConfig<P>): OAuthConfig<P> {
+  return {
+    id: 'orcid',
+    name: 'ORCID',
+    type: 'oauth',
+    wellKnown: "https://orcid.org/.well-known/openid-configuration",
+    authorization: { params: { scope: "openid" } },
+    idToken: true,
+    checks: ["pkce", "state"],
+    profile(profile) {
+      return {
+        id: profile.sub,
+        name: profile.name,
+        email: profile.email,
+      }
+    },
+    style: {
+      logo: 'https://orcid.org/assets/vectors/orcid.logo.icon.svg',
+      logoDark: 'https://orcid.org/assets/vectors/orcid.logo.icon.svg',
+      bgDark: "#fff",
+      bg: "#fff",
+      text: "#000",
+      textDark: "#000",
+    },
+    options,
+  }
+}
--- app/pages/api/auth/[...nextauth].ts
+++ app/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import type { Provider } from "next-auth/providers"
 import EmailProvider from 'next-auth/providers/email'
 import GoogleProvider from "next-auth/providers/google"
 import ORMAdapter from '@/app/extensions/next-auth/orm-adapter'
+import ORCIDProvider from '@/app/extensions/next-auth/orcid-provider'
 
 export type AuthOptions = typeof authOptions
 export type SessionWithId = Session & { user: { id: string } }
@@ -11,6 +12,7 @@ export const authOptions: NextAuthOptions = {
   adapter: ORMAdapter(),
   providers: ([
     process.env.NEXTAUTH_GOOGLE ? GoogleProvider(JSON.parse(process.env.NEXTAUTH_GOOGLE)) : undefined,
+    process.env.NEXTAUTH_ORCID ? ORCIDProvider(JSON.parse(process.env.NEXTAUTH_ORCID)) : undefined,
     process.env.EMAIL_SERVER && process.env.EMAIL_FROM ? EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,

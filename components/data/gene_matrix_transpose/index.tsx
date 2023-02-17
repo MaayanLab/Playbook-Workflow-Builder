d961bfeb3967e152135cd162171f5b93b8d54234
Daniel Clarke <u8sand@gmail.com>
2023-02-17 18:33:55 -0500

    Turn GenesetsToGMT into Prompt to capture labels

--- components/data/gene_matrix_transpose/index.tsx
+++ components/data/gene_matrix_transpose/index.tsx
@@ -1,10 +1,12 @@
+import React from 'react'
 import { MetaNode } from '@/spec/metanode'
 import { z } from 'zod'
 import * as dict from '@/utils/dict'
 import * as array from '@/utils/array'
-import { Table, Cell, Column } from '@/app/components/Table'
+import { Table, Cell, Column, EditableCell } from '@/app/components/Table'
 import { GeneSet } from '@/components/core/input/set'
 import { downloadBlob } from '@/utils/download'
+import { Button } from '@blueprintjs/core'
 
 export const GMT = MetaNode(`GMT`)
   .meta({
@@ -95,8 +97,66 @@ export const GenesetsToGMT = MetaNode('GenesetsToGMT')
   })
   .inputs({ genesets: [GeneSet] })
   .output(GMT)
-  .resolve(async (props) => {
-    return dict.init(props.inputs.genesets.map((set, i) => ({ key: i+'', value: { set } })))
+  .prompt(props => {
+    const [terms, setTerms] = React.useState(dict.init(array.arange(props.inputs.genesets.length).map(key => ({ key, value: '' }))))
+    const [descriptions, setDescriptions] = React.useState(dict.init(array.arange(props.inputs.genesets.length).map(key => ({ key, value: '' }))))
+    React.useEffect(() => {
+      if (props.output) {
+        setTerms(dict.init(dict.keys(props.output).map((key, i) => ({ key: i, value: key }))))
+        setDescriptions(dict.init(dict.values(props.output).map(({ description }, i) => ({ key: i, value: description||'' }))))
+      }
+    }, [props.inputs, props.output])
+    return (
+      <div>
+        <Table
+          height={500}
+          cellRendererDependencies={[props.inputs.genesets, terms, descriptions]}
+          numRows={props.inputs.genesets.length}
+          enableGhostCells
+          enableFocusedCell
+        >
+          <Column
+            name="Term"
+            cellRenderer={row => <EditableCell
+              key={row+''}
+              value={terms[row]}
+              placeholder={`Geneset from path ${row}`}
+              onChange={value => setTerms(terms => ({ ...terms, [row]: value }))}
+            />}
+          />
+          <Column
+            name="Descrition"
+            cellRenderer={row => <EditableCell
+              key={row+''}
+              value={descriptions[row]}
+              placeholder={`Some optional description`}
+              onChange={value => setDescriptions(descriptions => ({ ...descriptions, [row]: value }))}
+            />}
+          />
+          <Column
+            name="Geneset"
+            cellRenderer={row => <Cell key={row+''}>{props.inputs.genesets[row].join('\t')}</Cell>}
+          />
+        </Table>
+        <Button
+          large
+          type="submit"
+          text="Submit"
+          rightIcon="bring-data"
+          onClick={() => props.submit(
+            dict.init(
+              array.arange(props.inputs.genesets.length)
+                .map(i => ({
+                  key: terms[i],
+                  value: {
+                    description: descriptions[i],
+                    set: props.inputs.genesets[i],
+                  }
+                }))
+            ))}
+        />
+      </div>
+    )
   })
   .build()
 

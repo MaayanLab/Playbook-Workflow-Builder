729e7947d1c425eaeeecd0e301f725113bb20257
Daniel Clarke <u8sand@gmail.com>
2023-02-21 17:51:32 -0500

    Fix upload errors caused by authenticated upload endpoint

--- app/extensions/next-auth/helpers.ts
+++ app/extensions/next-auth/helpers.ts
@@ -4,5 +4,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import type { SessionWithId } from '@/app/pages/api/auth/[...nextauth]'
 
 export async function getServerSessionWithId(req: NextApiRequest, res: NextApiResponse) {
+  if (req.headers.authorization === `Token ${process.env.NEXTAUTH_SECRET}`) {
+    // worker authentication
+    // TODO: workers should work on behalf of the user who triggered them
+    return {
+      user: {
+        id: '00000000-0000-0000-0000-000000000000'
+      }
+    }
+  }
   return await getServerSession(req, res, authOptions) as SessionWithId | null
 }

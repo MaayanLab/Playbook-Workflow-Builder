8e936c7d61da2e706ab8d501129272458b09601d
Daniel Clarke <u8sand@gmail.com>
2023-02-28 17:35:52 -0500

    [tsvector] Improvements

--- utils/tsvector.ts
+++ utils/tsvector.ts
@@ -1,4 +1,4 @@
-class TSVector {
+export class TSVector {
   constructor(private set = new Set<string>()) {}
   add = (other: string) => {
     this.set.add(other)
@@ -20,11 +20,27 @@ class TSVector {
     return this.set.size
   }
 }
+
+/**
+ * tri-gram searching works as follows:
+ * Given: "my text"
+ *  we produce tokens like so: "  my  text " => {"  m", " my", "my ", "  t", " te", ...}
+ * Then we do the same to the search and compute a set overlap.
+ * Two spaces are used in front of a word boundary to capture single letter prefix matching,
+ *   but two spaces are not used for the postfix of a word boundary.
+ * This variant of trigram searching seems to produce the most intuitive results when using
+ *  1 or 2 characters for the search.
+ */
 export default function tsvector(s: string) {
-  const s_split = ` ${s.toLowerCase().replace(/^\s+/g, '').replace(/\s+$/g, '').replace(/\s+/g, ' ')} `.split('')
+  s = s.toLowerCase()
+    .replace(/\s+/g, '  ')
+    .replace(/^\s*/g, s.length > 1 ? ' ' : '  ')
+    .replace(/\s*$/g, '  ')
   const trigrams = new TSVector()
-  for (let i = 0, j = 3; j < s.length; i++, j++) {
-    trigrams.add(s_split.slice(i, j).join(''))
+  for (let i = 0, j = 3; j <= s.length; i++, j++) {
+    const ngram = s.slice(i, j)
+    if (ngram.endsWith('  ')) continue
+    trigrams.add(ngram)
   }
   return trigrams
 }

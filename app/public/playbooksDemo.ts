8fb80d990beb6aaafd4e48e31a0956234c012791
Daniel Clarke <u8sand@gmail.com>
2023-02-28 17:06:41 -0500

    [playbooks]: Add collapsible details

--- app/public/playbooksDemo.ts
+++ app/public/playbooksDemo.ts
@@ -5,6 +5,11 @@ const playbooks = [
     id: '9abb4a04-8bcd-4ecf-1628-6c9821e16ccc',
     label: 'GTEx Tissue Enrichment of Transcription Factor Targets',
     description: 'Given a Transcription Factor, we collect its targets by various resources and then Enrich the set of consensus targets against GTEx Tissue expression. The result is ranked tissues potentially regulated by the transcription factor.',
+    published: 'Feb 27, 2023',
+    version: '1.0.0',
+    authors: ['CFDE Playbook Partnership'],
+    licenseUrl: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+    license: 'CB-BY-NC-SA-4.0',
     url: 'https://playbook-partnership.maayanlab.cloud/report/9abb4a04-8bcd-4ecf-1628-6c9821e16ccc',
     dataSources: [
       'ENCODE',
@@ -23,6 +28,11 @@ const playbooks = [
     id: 'd30f6646-f29b-4427-41bb-641be48805a4',
     label: 'Explain MOAs of Side Effects for Approved Drugs',
     description: 'For a side effect and a drug, I would like to know if there are genes from the LINCS L1000 experiments that are up or down-regulated by the drugs that are also known to be involved with the side effect based on literature co-mentions or GWAS. I would like to know if such overlap is statistically significant. I would also like the results to be visualized using a SuperVenn diagram.',
+    published: 'Feb 27, 2023',
+    version: '1.0.0',
+    authors: ['CFDE Playbook Partnership'],
+    licenseUrl: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+    license: 'CB-BY-NC-SA-4.0',
     url: 'https://playbook-partnership.maayanlab.cloud/report/d30f6646-f29b-4427-41bb-641be48805a4',
     dataSources: [
       'GWAS Catalog',
@@ -39,4 +49,5 @@ const playbooks = [
     ],
   },
 ]
+
 export default playbooks

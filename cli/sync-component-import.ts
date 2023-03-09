bc14bceeb888ff75ac9328ee5e7f338b6442d365
Daniel Clarke <u8sand@gmail.com>
2023-03-09 12:43:06 -0500

    Fix component/index.ts generation on windows

--- cli/sync-component-import.ts
+++ cli/sync-component-import.ts
@@ -3,7 +3,7 @@ import glob from 'glob'
 import path from 'path'
 
 const base = path.join(__dirname, '..', 'components')
-const components = glob.sync(path.join(base, '**/package.json'))
+const components = glob.sync(path.join(base, '**', 'package.json').replace(/\\/g, '/'))
   .map(p => path.dirname(p))
 components.sort()
 

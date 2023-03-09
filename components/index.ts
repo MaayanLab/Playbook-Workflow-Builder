d036d9a020464c90dd6421e9905a780e8e8d4d4a
Miroslav Milinkov <miroslav.milinkov@persida-bio.com>
2023-03-09 18:29:57 +0100

    Corrected errors for packages, Variant module is now working

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,65 @@
 export const components: string[] = []
+export * from "./MW/ConvertedGeneID"
+components.push("MW/ConvertedGeneID")
+export * from "./MW/geneidconv"
+components.push("MW/geneidconv")
+export * from "./MW/metaboliteInfo"
+components.push("MW/metaboliteInfo")
+export * from "./MW/metabolite_summary"
+components.push("MW/metabolite_summary")
+export * from "./MW/metgene_metabolite_table"
+components.push("MW/metgene_metabolite_table")
+export * from "./MW/metgene_metabolites"
+components.push("MW/metgene_metabolites")
+export * from "./MW/metgene_rxn_table"
+components.push("MW/metgene_rxn_table")
+export * from "./MW/metgene_rxns"
+components.push("MW/metgene_rxns")
+export * from "./MW/metgene_studies"
+components.push("MW/metgene_studies")
+export * from "./MW/metgene_study_table"
+components.push("MW/metgene_study_table")
+export * from "./core/error"
+components.push("core/error")
+export * from "./core/file"
+components.push("core/file")
+export * from "./core/input/scored"
+components.push("core/input/scored")
+export * from "./core/input/set"
+components.push("core/input/set")
+export * from "./core/input/term"
+components.push("core/input/term")
+export * from "./data/gene_count_matrix"
+components.push("data/gene_count_matrix")
+export * from "./data/gene_matrix_transpose"
+components.push("data/gene_matrix_transpose")
+export * from "./data/log_normalization"
+components.push("data/log_normalization")
+export * from "./data/pca_transformation"
+components.push("data/pca_transformation")
+export * from "./data/quantile_normalization"
+components.push("data/quantile_normalization")
+export * from "./data/tsne_transformation"
+components.push("data/tsne_transformation")
+export * from "./data/umap_transformation"
+components.push("data/umap_transformation")
+export * from "./data/z_score_normalization"
+components.push("data/z_score_normalization")
+export * from "./gly_gen"
+components.push("gly_gen")
+export * from "./service/enrichr"
+components.push("service/enrichr")
+export * from "./service/gtex"
+components.push("service/gtex")
+export * from "./service/hyposet"
+components.push("service/hyposet")
+export * from "./service/mygeneinfo"
+components.push("service/mygeneinfo")
+export * from "./service/variantinfo"
+components.push("service/variantinfo")
+export * from "./viz/bokeh"
+components.push("viz/bokeh")
+export * from "./viz/plotly"
+components.push("viz/plotly")
+export * from "./viz/tissue_barplot"
+components.push("viz/tissue_barplot")

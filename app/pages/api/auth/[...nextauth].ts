814d3d7086576247dcba7828840015e7fa76a60d
Daniel J. B. Clarke <u8sand@gmail.com>
2023-03-15 15:33:00 -0400

    Add dev user for dev mode

--- app/pages/api/auth/[...nextauth].ts
+++ app/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth, { NextAuthOptions, Session } from 'next-auth'
 import type { Provider } from "next-auth/providers"
+import db from '@/app/db'
+import CredentialsProvider from 'next-auth/providers/credentials'
 import EmailProvider from 'next-auth/providers/email'
 import GoogleProvider from "next-auth/providers/google"
 import ORMAdapter from '@/app/extensions/next-auth/orm-adapter'
@@ -11,6 +13,16 @@ export type SessionWithId = Session & { user: { id: string } }
 export const authOptions: NextAuthOptions = {
   adapter: ORMAdapter(),
   providers: ([
+    process.env.NODE_ENV === 'development' ? CredentialsProvider({
+      id: 'dev',
+      name: 'Development User',
+      credentials: {},
+      async authorize(credentials, req) {
+        return {
+          id: '00000000-0000-0000-0000-000000000000'
+        }
+      }
+    }) : undefined,
     process.env.NEXTAUTH_GOOGLE ? GoogleProvider(JSON.parse(process.env.NEXTAUTH_GOOGLE)) : undefined,
     process.env.NEXTAUTH_ORCID ? ORCIDProvider(JSON.parse(process.env.NEXTAUTH_ORCID)) : undefined,
     process.env.EMAIL_SERVER && process.env.EMAIL_FROM ? EmailProvider({
@@ -19,13 +31,19 @@ export const authOptions: NextAuthOptions = {
     }) : undefined,
   ] as Array<Provider | undefined>).filter((v): v is Provider => v !== undefined),
   callbacks: {
-    session: async ({ session, user }) => {
-      if (session?.user && user) Object.assign(session.user, { id: user.id })
+    session: async ({ session, token }) => {
+      Object.assign(session, {
+        user: await db.objects.user.findUnique({
+          where: {
+            id: token.sub,
+          }
+        })
+      })
       return session as SessionWithId
     }
   },
   session: {
-    strategy: 'database',
+    strategy: 'jwt',
   },
 }
 

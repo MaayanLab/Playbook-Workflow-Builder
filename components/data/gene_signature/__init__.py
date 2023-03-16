b2a6056b5eacb37af4d181d7ab6b75c7aaec6fb5
Sherry Xie <39966933+sxie04@users.noreply.github.com>
2023-03-16 17:26:55 -0400

    add GeneSignature metanode

---
+++ components/data/gene_signature/__init__.py
@@ -0,0 +1,85 @@
+import re
+import contextlib
+import fsspec
+import numpy as np
+import pandas as pd
+
+@contextlib.contextmanager
+def fsspec_open_as_iterator(url, *args, **kwargs) -> str:
+  with fsspec.open(url, *args, **kwargs) as fr:
+    yield fr
+
+@contextlib.contextmanager
+def fsspec_open_as_path(url, *args, **kwargs) -> str:
+  m = re.match(r'^file://(.+)$', url)
+  assert m, 'protocol not yet supported'
+  yield m.group(1)
+
+def signature_from_path(path):
+  ''' Read from a bunch of different formats, get a metadata table
+  '''
+  if path.endswith('.csv'):
+    with fsspec_open_as_iterator(path, 'r') as fr:
+      return pd.read_csv(fr, index_col=0)
+  elif path.endswith('.tsv'):
+    with fsspec_open_as_iterator(path, 'r') as fr:
+      return pd.read_csv(fr, sep='\t', index_col=0)
+  elif path.endswith('.txt') or path.endswith('.tab') or path.endswith('.data'):
+    with fsspec_open_as_iterator(path, 'r') as fr:
+      return pd.read_csv(fr, sep=None, index_col=0)
+  elif path.endswith('.xlsx'):
+    with fsspec_open_as_path(path, 'r') as fr:
+      return pd.read_excel(fr, index_col=0)
+  else:
+    raise NotImplementedError
+
+def np_jsonifyable(x):
+  x_ = x.astype('object')
+  return x_.tolist()
+
+def gene_signature(url):
+  d = signature_from_path(url)
+  if d.shape[0] >= 10:
+    top = 5
+    bottom = 5
+  elif d.shape[0] > 5:
+    top = 5
+    bottom = d.shape[0] - top
+  else:
+    top = d.shape[0] - 1
+    bottom = 1
+
+  index = np.concatenate([d.index[:top], d.index[-bottom:]]).tolist()
+  columns = d.columns.tolist()
+  values = np_jsonifyable(np.concatenate([
+    d.iloc[:top], d.iloc[-bottom:]
+  ]))
+  ellipses = [
+    top if len(index) != d.shape[0] else None, 
+    None,
+  ]
+  return dict(
+    url=url,
+    shape=d.shape,
+    index=index,
+    columns=columns,
+    values=values,
+    ellipses=ellipses,
+  )
+
+def gmt_from_sig(sig):
+  d = signature_from_path(sig['url'])
+  col = d.columns[0]
+  comparison = col.split('vs.')[0].strip() + ' vs. ' + col.split('vs.')[1].split(':')[0].strip()
+  up_250 = d.sort_values(by=col, ascending=False)[:250].index.tolist()
+  down_250 = d.sort_values(by=col, ascending=True)[:250].index.tolist()
+  return {
+    f'{comparison} Up Genes': {
+      'description': f'Top 250 up genes for {comparison}',
+      'set': up_250
+    },
+    f'{comparison} Down Genes': {
+      'description': f'Top 250 down genes for {comparison}',
+      'set': down_250
+    }
+  }

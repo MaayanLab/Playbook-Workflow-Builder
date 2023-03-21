24798c42e012a61eadebe74e4b08e4dccf99cf33
Daniel Clarke <u8sand@gmail.com>
2023-03-21 17:34:41 -0400

    [clustergrammer2]: Fit in container

--- components/viz/clustergrammer2/clustergrammer2.tsx
+++ components/viz/clustergrammer2/clustergrammer2.tsx
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid'
 
 export type Clustergrammer2Data = Omit<Parameters<typeof CGM>[0], 'root'>
 
-export default function ReactCGM(props: Clustergrammer2Data) {
+export default function ReactCGM({ viz_width = 900, viz_height = 1035, ...props }: Clustergrammer2Data) {
   const ref = React.useRef<HTMLDivElement>(null)
   React.useEffect(() => {
     if (typeof window === 'undefined' || !ref || !ref.current || !props.network) return
@@ -12,12 +12,7 @@ export default function ReactCGM(props: Clustergrammer2Data) {
     const container = document.createElement('div')
     container.setAttribute('id', id)
     ref.current.appendChild(container)
-    const {
-      viz_width = 900,
-      viz_height = 1035,
-      use_hzome = true,
-      ...props_
-    } = props
+    const { use_hzome = true, ...props_ } = props
     CGM({
       viz_width,
       viz_height,
@@ -28,8 +23,8 @@ export default function ReactCGM(props: Clustergrammer2Data) {
     return () => {
       if (ref && ref.current) ref.current.removeChild(container)
     }
-  }, [ref, props])
+  }, [ref, props, viz_width, viz_height])
   return (
-    <div ref={ref} className="flex-grow overflow-scroll relative" />
+    <div ref={ref} style={{ width: `${viz_width}px`, height: `${viz_height + 150}px`, margin: 'auto' }} />
   )
 }

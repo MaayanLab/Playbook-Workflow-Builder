f83a2921bf9c81dfc2bafd4dbe6fa11b4c1c6446
Daniel Clarke <u8sand@gmail.com>
2023-03-21 13:45:06 -0400

    Improvement to python helper (don't fail on spurious stdout printing)

--- utils/helper.py
+++ utils/helper.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # Usage: ./python_helper.py mod.path.func_name <<< '{"kargs": ["value"]}' >>> '{"output": "json"}'
-import sys, json, pathlib, importlib
+import sys, json, pathlib, importlib, contextlib
 _, pathspec = sys.argv
 name, _, attr = pathspec.rpartition('.')
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.absolute()))
-mod = importlib.import_module(name)
-func = getattr(mod, attr)
-args = json.load(sys.stdin)
-out = func(*(args.get('kargs') or []), **(args.get('kwargs') or {}))
+with contextlib.redirect_stdout(sys.stderr):
+  mod = importlib.import_module(name)
+  func = getattr(mod, attr)
+  args = json.load(sys.stdin)
+  out = func(*(args.get('kargs') or []), **(args.get('kwargs') or {}))
 json.dump(out, sys.stdout, allow_nan=False)

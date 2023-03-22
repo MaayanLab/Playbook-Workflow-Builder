351b36e044859309fdbcaa93a819c583cf87eabd
Daniel Clarke <u8sand@gmail.com>
2023-03-22 11:31:01 -0400

    [report]: Capture complete story into a variable

--- app/fragments/report/cells.tsx
+++ app/fragments/report/cells.tsx
@@ -1,87 +1,23 @@
-import React from 'react'
 import dynamic from 'next/dynamic'
 import type KRG from '@/core/KRG'
-import Link from 'next/link'
-import { view_in_graph_icon, fork_icon, start_icon } from '@/icons'
+import type { Metapath } from '@/app/fragments/metapath'
 import { useSWRImmutableSticky } from '@/utils/use-sticky'
-import { Metapath } from '@/app/fragments/metapath'
-import classNames from 'classnames'
+import { StoryProvider } from '@/app/fragments/report/story'
 
-const ShareButton = dynamic(() => import('@/app/fragments/report/share-button'))
+const Introduction = dynamic(() => import('@/app/fragments/report/introduction'))
 const Cell = dynamic(() => import('@/app/fragments/report/cell'))
-const Story = dynamic(() => import('@/app/fragments/report/story'))
-const EditableText = dynamic(() => import('@blueprintjs/core').then(({ EditableText }) => EditableText))
-const Icon = dynamic(() => import('@/app/components/icon'))
 
 export default function Cells({ krg, id }: { krg: KRG, id: string }) {
-  const descriptionRef = React.useRef<HTMLTextAreaElement>(null)
   const { data: metapath, error } = useSWRImmutableSticky<Array<Metapath>>(id ? `/api/db/fpl/${id}` : undefined)
-  const [metadata, setMetadata] = React.useState({ title: '', description: undefined as string | undefined, public: false })
+  if (!metapath) return null
   return (
     <div className="flex flex-col py-4 gap-2">
-      <div className="flex-grow flex-shrink bp4-card p-0">
-        <div className="p-3">
-          <div className="flex flex-row gap-2">
-            <Icon icon={start_icon} />
-            <h2 className="bp4-heading">
-              <EditableText
-                placeholder="Playbook title"
-                value={metadata.title}
-                onChange={value => {setMetadata(metadata => ({ ...metadata, title: value }))}}
-              />
-            </h2>
-          </div>
-          <p
-            className={classNames('prose w-full p-1 border border-white hover:border-gray-200 cursor-text', { 'hidden': metadata.description !== undefined })}
-            onClick={evt => {
-              setMetadata(metadata => ({ ...metadata, description: evt.currentTarget?.textContent||''.replace(/\s+$/, '') }))
-              setTimeout(() => {
-                descriptionRef.current?.focus()
-                descriptionRef.current?.click()
-              }, 100)
-            }}
-          >
-            {(metapath||[]).map(head => <Story key={head.id} krg={krg} head={head} />)}
-          </p>
-          <textarea
-            ref={descriptionRef}
-            className={classNames('prose w-full p-1 border border-white', { 'hidden': metadata.description === undefined })}
-            value={metadata.description}
-            onChange={evt => {
-              setMetadata(metadata => ({ ...metadata, description: evt.target.value }))
-            }}
-          />
-          {metadata.title || metadata.description !== undefined ? (
-            <div className="flex flex-row gap-2 items-center">
-              <button className="bp4-button bp4-intent-success">Save</button>
-              <label className="bp4-control bp4-switch mb-0">
-                <input
-                  type="checkbox"
-                />
-                <span className="bp4-control-indicator"></span>
-                Make workflow public
-              </label>
-            </div>
-          ) : null}
-        </div>
-        {error ? <div className="alert alert-error">{error}</div> : null}
-        <div className="border-t-secondary border-t-2 mt-2">
-          <Link href={`/graph${id ? `/${id}/node/start` : ``}`}>
-            <button className="bp4-button bp4-minimal">
-              <Icon icon={view_in_graph_icon} />
-            </button>
-          </Link>
-          <Link href={`/graph${id ? `/${id}/node/start/extend` : `/start/extend`}`}>
-            <button className="bp4-button bp4-minimal">
-              <Icon icon={fork_icon} color="black" />
-            </button>
-          </Link>
-          <ShareButton id={id} />
-        </div>
-      </div>
-      {(metapath||[]).map((head, index) => (
-        <Cell key={head.id} krg={krg} id={id} head={head} defaultCollapse={index+1 !== metapath?.length} />
-      ))}
+      <StoryProvider krg={krg} metapath={metapath}>
+        <Introduction id={id} error={error} />
+        {(metapath||[]).map((head, index) => (
+          <Cell key={head.id} krg={krg} id={id} head={head} defaultCollapse={index+1 !== metapath?.length} />
+        ))}
+      </StoryProvider>
     </div>
   )
 }
--- app/fragments/report/story.tsx
+++ app/fragments/report/story.tsx
@@ -1,8 +1,20 @@
+/**
+ * This is a bit hacky but the nicer way of dealing with stories.
+ * The main issue is that useMetapathInputs/useMetapathOutputs relies on SWR which only works
+ *  as a React Hook. So it becomes a lot simpler to have independent processes independently
+ *  assemble the story into one element -- then we can just extract the full story from the
+ *  parent DOM element. This is nicer than waiting for the full story to be ready and doesn't
+ *  require us to re-implement SWR.
+ */
+import React from 'react'
 import type KRG from '@/core/KRG'
 import type { Metapath } from '@/app/fragments/metapath'
 import { useMetapathInputs, useMetapathOutput } from '@/app/fragments/metapath'
 
-export default function Story({ krg, head }: { krg: KRG, head: Metapath }) {
+/**
+ * Attempt to compute the story for any given step
+ */
+function StoryNode({ krg, head }: { krg: KRG, head: Metapath }) {
   const processNode = krg.getProcessNode(head.process.type)
   const { data: inputs } = useMetapathInputs(krg, head)
   const { data: { output } } = useMetapathOutput(krg, head)
@@ -19,4 +31,42 @@ export default function Story({ krg, head }: { krg: KRG, head: Metapath }) {
   } catch (e) {
     return null
   }
+}
+
+const StoryContext = React.createContext('')
+
+/**
+ * We construct the story in a hidden element and sync changes to that element
+ *  to a state variable which we expose through a context variable
+ */
+export function StoryProvider({ children, metapath, krg }: React.PropsWithChildren<{ metapath: Metapath[], krg: KRG }>) {
+  const ref = React.useRef<HTMLSpanElement>(null)
+  const [story, setStory] = React.useState('')
+  React.useEffect(() => {
+    if (!ref || !ref.current) return
+    const observer = new MutationObserver(() => {
+      if (!ref || !ref.current) return
+      setStory(() => ref.current?.textContent || '')
+    })
+    observer.observe(ref.current, { childList: true, subtree: true })
+    return () => {observer.disconnect()}
+  }, [ref, ref.current])
+  return (
+    <>
+      <StoryContext.Provider value={story}>
+        {children}
+      </StoryContext.Provider>
+      <span ref={ref} className="hidden">
+        {(metapath||[]).map(head => <StoryNode key={head.id} krg={krg} head={head} />)}
+      </span>
+    </>
+  )
+}
+
+/**
+ * Any descendent of StoryProvider can access the full story text
+ *  with useStory
+ */
+export function useStory() {
+  return React.useContext(StoryContext)
 }

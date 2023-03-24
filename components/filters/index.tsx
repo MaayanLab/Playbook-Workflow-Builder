6ebe68fde7f9959f691f644eb84596a2a6f76cd8
Daniel Clarke <u8sand@gmail.com>
2023-03-24 16:02:29 -0400

    [filter]: Add single term selection

--- components/filters/index.tsx
+++ components/filters/index.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import { MetaNode } from '@/spec/metanode'
 import { DrugSet, GeneSet } from '@/components/core/input/set'
+import { Disease, Drug, Gene, Pathway, Phenotype, Tissue } from '@/components/core/input/primitives'
+import { DiseaseTerm, DrugTerm, GeneTerm, PathwayTerm, PhenotypeTerm, TissueTerm } from '@/components/core/input/term'
 import { ScoredDrugs, ScoredGenes, ScoredDiseases, ScoredPathways, ScoredPhenotypes, ScoredTissues } from '@/components/core/input/scored'
+import { Table, Cell, Column } from '@/app/components/Table'
 import classNames from 'classnames'
 
+function withPrecision(value: number | string, precision: number) {
+  if (typeof value === 'number') return value.toPrecision(precision)
+  else return value
+}
+
 export const TopKScoredT = [
-  ScoredDrugs,
-  ScoredGenes,
-  ScoredDiseases,
-  ScoredPathways,
-  ScoredPhenotypes,
-  ScoredTissues,
-].map((ScoredT) =>
+  { ScoredT: ScoredDrugs, TermT: DrugTerm, T: Drug },
+  { ScoredT: ScoredGenes, TermT: GeneTerm, T: Gene },
+  { ScoredT: ScoredDiseases, TermT: DiseaseTerm, T: Disease },
+  { ScoredT: ScoredPathways, TermT: PathwayTerm, T: Pathway },
+  { ScoredT: ScoredPhenotypes, TermT: PhenotypeTerm, T: Phenotype },
+  { ScoredT: ScoredTissues, TermT: TissueTerm, T: Tissue },
+].flatMap(({ ScoredT, TermT, T }) => [
   MetaNode(`TopKScoredT[${ScoredT.spec}]`)
     .meta({
       label: `Top ${ScoredT.meta.label}`,
@@ -48,8 +56,64 @@ export const TopKScoredT = [
       )
     })
     .story(props => `The top ${props.output?.length || 'K'} ${ScoredT.meta.label} were selected.`)
+    .build(),
+  MetaNode(`OneScoredT[${ScoredT.spec}]`)
+    .meta({
+      label: `Select One ${TermT.meta.label}`,
+      description: `Select one ${TermT.meta.label}`,
+    })
+    .inputs({ scored: ScoredT })
+    .output(TermT)
+    .prompt(props => {
+      const scored = props.inputs.scored
+      const [selected, setSelected] = React.useState<number | undefined>()
+      React.useEffect(() => {
+        if (props.output !== undefined) {
+          const index = scored.findIndex(({ term }) => term === props.output)
+          if (index >= 0) {
+            setSelected(index)
+            return
+          }
+        }
+        if (scored.length > 0) {
+          setSelected(0)
+        }
+      }, [props.output, scored])
+      return (
+        <div>
+          <Table
+            height={500}
+            cellRendererDependencies={[scored]}
+            rowHeaderCellRenderer={(row) =>
+              <div
+                className="text-center block"
+                onClick={evt => {
+                  setSelected(row)
+                  props.submit(scored[row].term)
+                }}
+              >
+                <input type="radio" checked={selected === row} />
+              </div>
+            }
+            numRows={scored.length}
+            enableGhostCells
+            enableFocusedCell
+          >
+            <Column
+              name={T.label}
+              cellRenderer={row => <Cell key={row+''}>{scored[row].term}</Cell>}
+            />
+            <Column
+              name="ZScore"
+              cellRenderer={row => <Cell key={row+''}>{withPrecision(scored[row].zscore, 3)}</Cell>}
+            />
+          </Table>
+        </div>
+      )
+    })
+    .story(props => props.output ? `${props.output} was chosen for further investigation.` : '')
     .build()
-)
+])
 
 export const SetFromScoredT = [
   { ScoredT: ScoredDrugs, SetT: DrugSet, },

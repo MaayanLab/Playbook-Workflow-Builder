d1a0216ed6b2fbddc9316ae95f08890d43a51d16
Daniel Clarke <u8sand@gmail.com>
2023-03-30 13:22:45 -0400

    [idg]: Include all dark proteins filter

--- components/service/idg/index.tsx
+++ components/service/idg/index.tsx
@@ -2,19 +2,21 @@ import { ScoredGenes } from '@/components/core/input/scored'
 import { GeneSet } from '@/components/core/input/set'
 import { idg_icon } from '@/icons'
 import { MetaNode } from '@/spec/metanode'
+import * as dict from '@/utils/dict'
 
 export const IDGFilterT = [
-  { kind: 'kinase', label: 'Kinase', },
-  { kind: 'gpcr', label: 'GPCR', },
-  { kind: 'ionchannel', label: 'Ion Channel', },
-  { kind: 'darkkinase', label: 'Dark Kinase', },
-  { kind: 'darkgpcr', label: 'Dark GPCR', },
-  { kind: 'darkionchannel', label: 'Dark Ion Channel', },
+  { kind: 'all', label: 'Understudied Proteins', },
+  { kind: 'kinase', label: 'Kinases', },
+  { kind: 'gpcr', label: 'GPCRs', },
+  { kind: 'ionchannel', label: 'Ion Channels', },
+  { kind: 'darkkinase', label: 'Dark Kinases', },
+  { kind: 'darkgpcr', label: 'Dark GPCRs', },
+  { kind: 'darkionchannel', label: 'Dark Ion Channels', },
 ].flatMap(({ kind, label }) => [
   MetaNode(`IDGFilter[${ScoredGenes.spec}, ${kind}]`)
     .meta({
       label: `Filter genes by ${label}`,
-      description: `Filter genes by IDG ${label} classification`,
+      description: `Based on IDG understudied proteins list`,
       icon: [idg_icon],
       pagerank: -1,
     })
@@ -22,16 +24,24 @@ export const IDGFilterT = [
     .output(ScoredGenes)
     .resolve(async (props) => {
       const req = await fetch('https://maayanlab.cloud/geneshot/api/idgclass')
-      const { idg: { [kind]: filter } } = await req.json()
+      let filter: string[]
+      const { idg } = await req.json()
+      if (kind === 'all') {
+        filter = dict.values(
+          dict.filter(idg, ({ key }) => (key as string).startsWith('dark'))
+        ).reduce((all, proteins) => [...all, ...proteins], [])
+      } else {
+        filter = idg[kind]
+      }
       const filterSet = new Set((filter as string[]).map(term => term.toLowerCase()))
       return props.inputs.input.filter(({ term }) => filterSet.has(term.toLocaleLowerCase()))
     })
-    .story(props => `Genes were filtered by those classified as ${label} by IDG.`)
+    .story(props => `Genes were filtered by IDG ${label}\\ref{IDG Understudied Proteins, https://druggablegenome.net/AboutIDGProteinList}.`)
     .build(),
   MetaNode(`IDGFilter[${GeneSet.spec}, ${kind}]`)
     .meta({
-      label: `Filter genes by ${label}`,
-      description: `Filter genes by IDG ${label} classification`,
+      label: `Filter genes by IDG ${label}`,
+      description: `Based on IDG understudied proteins list`,
       icon: [idg_icon],
       pagerank: -1,
     })
@@ -46,6 +56,6 @@ export const IDGFilterT = [
         set: props.inputs.input.set.filter((term) => filterSet.has(term.toLocaleLowerCase())),
       }
     })
-    .story(props => `Genes were filtered by those classified as ${label} by IDG.`)
+    .story(props => `Genes were filtered by IDG ${label}\\ref{IDG Understudied Proteins, https://druggablegenome.net/AboutIDGProteinList}.`)
     .build(),
 ])

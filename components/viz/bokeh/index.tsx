4ac2934dc28559b0893dac5a8a9a9e44387dfa12
Daniel Clarke <u8sand@gmail.com>
2023-04-06 16:02:27 -0400

    [bokeh]: Adopt plot icon

--- components/viz/bokeh/index.tsx
+++ components/viz/bokeh/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MetaNode } from '@/spec/metanode'
 import dynamic from 'next/dynamic'
+import { plot_icon } from '@/icons'
 
 const Plot = dynamic(() => import('./bokeh'), { ssr: false })
 
@@ -10,6 +11,7 @@ export const BokehPlot = MetaNode('BokehPlot')
   .meta({
     label: 'Bokeh Plot',
     description: 'A figure created with the Bokeh Library [https://bokeh.org/]',
+    icon: [plot_icon],
   })
   .codec<BokehJson>()
   .view(props => <Plot plot={props} />)

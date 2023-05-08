66e6d2641858ac31aacd759dc2bc3943fd403e7c
Daniel Clarke <u8sand@gmail.com>
2023-05-08 10:46:22 -0400

    Update build-table to include story and inputs/outputs

--- cli/build-table.ts
+++ cli/build-table.ts
@@ -1,28 +1,50 @@
 import KRG from '@/core/KRG'
 import { metanodes } from '@/components'
 import * as dict from '@/utils/dict'
+import { ProcessMetaNode } from '@/spec/metanode'
 
 const krg = new KRG()
 metanodes.forEach(metanode => krg.add(metanode))
 
+function slugify(spec: string) {
+  return spec.replaceAll(/[\[\]\(\)\{\} ,-]/g, '_').replaceAll(/_+$/g, '')
+}
+  
+function try_story<T extends ProcessMetaNode>(metanode: T, props: T['story'] extends (undefined | ((arg: infer A) => string)) ? A : never) {
+  try {
+    if (metanode.story) {
+      return metanode.story(props as any)
+    }
+  } catch (e) {}
+  return ''
+}
+
 console.log(
   [
-    ['id', 'type', 'input', 'output', 'label', 'description', 'color', 'story'].join('\t'),
+    ['id', 'slug', 'type', 'func', 'label', 'description', 'story'].join('\t'),
     ...[
       ...krg.getDataNodes(),
       ...krg.getPromptNodes(),
       ...krg.getResolveNodes(),
     ].map(node => [
       node.spec,
+      slugify(node.spec),
       'prompt' in node ? 'Prompt'
         : node.kind === 'process' ? 'Resolver'
         : 'Data',
-      node.kind === 'process' ? JSON.stringify(dict.init(dict.items(node.inputs).map(({ key, value }) => ({ key, value: Array.isArray(value) ? [value[0].spec] : value.spec })))) : '',
-      node.kind === 'process' ? node.output.spec : '',
+        node.kind === 'process' ? `${slugify(node.spec)}(${dict.items(node.inputs).map(({ key, value }) =>
+        `${key}: ${Array.isArray(value) ? `list[${slugify(value[0].spec)}]` : slugify(value.spec)}`,
+      ).join(', ')}) -> ${node.output.spec}` : '',
       node.meta.label,
       node.meta.description,
-      node.meta.color,
-      'story' in node ? node.story({}) : '',
+      'story' in node ? try_story(node, {
+        inputs: dict.init(
+          dict.items(node.inputs).map(({ key, value }) => ({
+            key, value: `{${Array.isArray(value) ? `[${slugify(value[0].spec)}]` : slugify(value.spec)}}`,
+          }))
+        ),
+        output: `{${slugify(node.output.spec)}}`,
+      }) : '',
     ].join('\t')),
   ].join('\n')
 )

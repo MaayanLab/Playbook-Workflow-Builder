4b3e78c232b35f69552025ca9a1f983e3732604e
Daniel Clarke <u8sand@gmail.com>
2023-05-16 17:18:06 -0400

    [refactoring]: Add type-safe API helper

---
+++ core/api/client.ts
@@ -0,0 +1,38 @@
+import React from 'react'
+import useSWR from 'swr'
+import useSWRMutation from 'swr/mutation'
+import * as dict from '@/utils/dict'
+import fetcher, { fetcherGET } from '@/utils/next-rest-fetcher'
+import { z } from 'zod'
+import type { APIRouteInterface } from "@/spec/api"
+
+export function useAPIQuery<Q extends {}, O>(route: APIRouteInterface<Q, O>, query: Q) {
+  const key = React.useMemo(() => {
+    let path = route.path
+    const searchParams = new URLSearchParams()
+    dict.items(query).forEach(({ key, value }) => {
+      const m = (new RegExp(`[\\.*${key as string}]`)).exec(path)
+      if (m !== null) path = path.replace(m[0], typeof value === 'string' ? value : JSON.stringify(value))
+      else searchParams.append(key as string, typeof value === 'string' ? value : JSON.stringify(value))
+    })
+    const params = searchParams.toString()
+    if (params) return path + '?' + params
+    else return path
+  }, [route.path, query])
+  return useSWR(key, fetcherGET<O>)
+}
+
+export function useAPIMutation<Q extends {}, O, B extends z.ZodType | FormData>(route: APIRouteInterface<Q, O, B>, query?: Partial<Q>) {
+  return useSWRMutation(route.path, (_key: RequestInfo | URL, { arg = {} }: { arg?: { query?: Partial<Q>, body?: B } }) => {
+    let path = route.path
+    const searchParams = new URLSearchParams()
+    dict.items({ ...query, ...(arg.query??{}) }).forEach(({ key, value }) => {
+      const m = (new RegExp(`\\[\\.*${key as string}\\]`, 'g')).exec(path)
+      if (m !== null) path = path.replace(m[0], typeof value === 'string' ? value : JSON.stringify(value))
+      else searchParams.append(key as string, typeof value === 'string' ? value : JSON.stringify(value))
+    })
+    const params = searchParams.toString()
+    if (params) path = path + '?' + params
+    return fetcher<O>(path, { method: 'POST', body: typeof arg.body === undefined ? undefined : arg.body instanceof FormData ? arg.body : JSON.stringify(arg.body) })
+  })
+}
---
+++ core/api/router.ts
@@ -0,0 +1,69 @@
+import { APIRoute } from "@/spec/api";
+import getRawBody from 'raw-body'
+import { NotFoundError, UnsupportedMethodError } from "@/spec/error";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default class APIRouter {
+  private routes: Record<string, Record<string, APIRoute>> = {}
+  private routeExprs: Record<string, RegExp> = {}
+
+  /**
+   * Add type-safe API route to in-memory lookup tables
+   */
+  add(route: APIRoute) {
+    if (!(route.path in this.routes)) this.routes[route.path] = {}
+    this.routes[route.path][route.method] = route
+    if (route.path in this.routeExprs) return
+    // convert the path into a RegExp if applicable
+    //  /hello/[variable]/world => new RegExp('/hello/(?<variable>[^/]+)/world')
+    let expr = /\[(?<arg>.+?)\]/g
+    let m = expr.exec(route.path)
+    let routeExpr = route.path
+    if (!m) return
+    while (m !== null) {
+      routeExpr = routeExpr.replace(
+        m[0],
+        m[1].startsWith('..') ? `(?<${m[1].replace(/^\.+/, '')}>.+)` : `(?<${m[1]}>[^/]+)`,
+      )
+      m = expr.exec(route.path)
+    }
+    this.routeExprs[route.path] = new RegExp(`^${routeExpr}$`)
+  }
+
+  /**
+   * Find the API path, resolving path parameter matching when applicable
+   */
+  find = (path: string) => {
+    if (path in this.routes) return { routes: this.routes[path], pathParams: {} }
+    const matches: { route: string, m: RegExpExecArray }[] = []
+    for (const route in this.routeExprs) {
+      const expr = this.routeExprs[route]
+      const m = expr.exec(path)
+      if (m) matches.push({ route, m })
+    }
+    if (matches.length > 1) matches.sort((a, b) => a.route.split('/').length - a.route.split('/').length)
+    else if (matches.length === 0) return
+    const match = matches[0]
+    return { routes: this.routes[match.route], pathParams: match.m.groups ?? {} }
+  }
+
+  /**
+   * Given a NextAPI request, actually call the relevant API and return its response
+   */
+  route = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (!req.url) throw new NotFoundError()
+    const url = new URL(req.url, `http://${req.headers.host}`)
+    const match = this.find(url.pathname)
+    if (typeof match === 'undefined') throw new NotFoundError()
+    if (!req.method || !(req.method in match.routes)) throw new UnsupportedMethodError()
+    const route = match.routes[req.method]
+    const query = route.parameters.parse({ ...match.pathParams, ...Object.fromEntries(url.searchParams.entries()) })
+    if (route.method === 'GET') {
+      return res.status(200).json(await route.call({ query }, req, res))
+    } else if (route.method === 'POST') {
+      const rawBody = (await getRawBody(req)).toString()
+      const body = route.requestBody.parse(rawBody ? JSON.parse(rawBody) : {})
+      return res.status(200).json(await route.call({ query, body }, req, res))
+    }
+  }
+}

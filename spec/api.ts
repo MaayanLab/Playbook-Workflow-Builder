3a2ca8e6a8bd8c5707a4a8f8fbbcdde58eb5aa48
Daniel Clarke <u8sand@gmail.com>
2023-05-16 17:18:06 -0400

    [refactoring]: Add type-safe API helper

---
+++ spec/api.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+
+export type APIRoute<Q extends {} = any, O = unknown, B extends z.ZodType | FormData = any> = 
+  { path: string, method: 'GET', parameters: z.ZodType<Q>, call: (input: { query: Q }, req: NextApiRequest, res: NextApiResponse) => Promise<O> }
+  | { path: string, method: 'POST', parameters: z.ZodType<Q>, requestBody: B, call: (input: { query: Q, body: B }, req: NextApiRequest, res: NextApiResponse) => Promise<O> }
+
+export type APIRouteInterface<Q extends {} = any, O = unknown, B extends z.ZodType | FormData = any> = 
+  { path: string, method: 'GET', call: z.ZodType<(input: { query: Q }, req: NextApiRequest, res: NextApiResponse) => Promise<O>> }
+  | { path: string, method: 'POST', call: z.ZodType<(input: { query: Q, body: B }, req: NextApiRequest, res: NextApiResponse) => Promise<O>> }
+
+export function API(path: string) {
+  return ({
+    query: <Q extends {}>(parameters: z.ZodType<Q>) => ({
+      call: <O>(call: (input: { query: Q }, req: NextApiRequest, res: NextApiResponse) => Promise<O>) => ({
+        build: () => ({ path, method: 'GET', parameters, call }) as APIRoute<Q, O>,
+      }),
+      body: <B extends z.ZodType | FormData>(requestBody: B) => ({
+        call: <O>(call: (input: { query: Q, body: B }, req: NextApiRequest, res: NextApiResponse) => Promise<O>) => ({
+          build: () => ({ path, method: 'POST', parameters, requestBody, call }) as APIRoute<Q, O, B>
+        })
+      }),
+    }),
+  })
+}

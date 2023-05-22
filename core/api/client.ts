fd9930bd29287b13ea6c47d4941a71b96803f535
Daniel J. B. Clarke <u8sand@gmail.com>
2023-05-22 12:36:46 -0400

    Passthrough additional args to useSWR from useAPI

--- core/api/client.ts
+++ core/api/client.ts
@@ -5,7 +5,9 @@ import * as dict from '@/utils/dict'
 import fetcher, { fetcherGET } from '@/utils/next-rest-fetcher'
 import type { APIRouteInterface } from "@/spec/api"
 
-export function useAPIQuery<Q extends {}, O>(route: APIRouteInterface<Q, O>, query: Q) {
+type FParamR<F> = F extends (param0: infer _P0, param1: infer _P1, ...rest: infer A) => infer _R ? A : never
+
+export function useAPIQuery<Q extends {}, O>(route: APIRouteInterface<Q, O>, query: Q, ...rest: FParamR<typeof useSWR<O>>) {
   const key = React.useMemo(() => {
     let path = route.path
     const searchParams = new URLSearchParams()
@@ -18,10 +20,10 @@ export function useAPIQuery<Q extends {}, O>(route: APIRouteInterface<Q, O>, que
     if (params) return path + '?' + params
     else return path
   }, [route.path, query])
-  return useSWR(key, fetcherGET<O>)
+  return useSWR(key, fetcherGET<O>, ...rest)
 }
 
-export function useAPIMutation<Q extends {}, O, B>(route: APIRouteInterface<Q, O, B>, query?: Partial<Q>) {
+export function useAPIMutation<Q extends {}, O, B>(route: APIRouteInterface<Q, O, B>, query?: Partial<Q>, ...rest: FParamR<typeof useSWRMutation<O>>) {
   return useSWRMutation(route.path, (_key: RequestInfo | URL, { arg = {} }: { arg?: { query?: Partial<Q>, body?: B } }) => {
     let path = route.path
     const searchParams = new URLSearchParams()
@@ -33,5 +35,5 @@ export function useAPIMutation<Q extends {}, O, B>(route: APIRouteInterface<Q, O
     const params = searchParams.toString()
     if (params) path = path + '?' + params
     return fetcher<O>(path, { method: 'POST', body: typeof arg.body === 'undefined' ? undefined : arg.body instanceof FormData ? arg.body : JSON.stringify(arg.body) })
-  })
+  }, ...rest)
 }

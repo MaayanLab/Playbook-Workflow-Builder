fe8ce9b24b85e4e46c213d6dcfdbf852b7a0ac70
Daniel J. B. Clarke <u8sand@gmail.com>
2023-05-22 16:59:43 -0400

    Update db with cell/playbook metadata

--- app/pages/api/db/fpl/[fpl_id]/rebaseMetadata/[rebase_id].ts
+++ app/pages/api/db/fpl/[fpl_id]/rebaseMetadata/[rebase_id].ts
@@ -1,6 +1,6 @@
 import fpprg from '@/app/fpprg'
 import { z } from 'zod'
-import { IdOrFPLMetadataC } from '@/core/FPPRG'
+import { IdOrPlaybookMetadataC } from '@/core/FPPRG'
 import handler from '@/utils/next-rest'
 import { NotFoundError, UnsupportedMethodError } from '@/spec/error'
 
@@ -9,17 +9,17 @@ const QueryType = z.object({
   rebase_id: z.string(),
 })
 
-const BodyType = IdOrFPLMetadataC
+const BodyType = IdOrPlaybookMetadataC
 
 export default handler(async (req, res) => {
   if (req.method !== 'POST') throw new UnsupportedMethodError()
   const { fpl_id, rebase_id } = QueryType.parse(req.query)
   const rebase_fpl = await fpprg.getFPL(rebase_id)
   if (rebase_fpl === undefined) throw new NotFoundError()
-  const new_metadata = await fpprg.resolveFPLMetadata(BodyType.parse(JSON.parse(req.body)))
+  const new_metadata = await fpprg.resolvePlaybookMetadata(BodyType.parse(JSON.parse(req.body)))
   const old_fpl = await fpprg.getFPL(fpl_id)
   if (old_fpl === undefined) throw new NotFoundError()
-  const { rebased, head } = old_fpl.rebaseMetadata(rebase_fpl, new_metadata)
+  const { rebased, head } = old_fpl.rebasePlaybookMetadata(rebase_fpl, new_metadata)
   const fpl = await fpprg.upsertFPL(head)
   res.status(200).json({ head: fpl.id, rebased: rebased.id })
 })

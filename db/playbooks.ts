6a4a8103aa205572ff27b2495c3f971310eba73c
Daniel Clarke <u8sand@gmail.com>
2023-05-30 11:48:31 -0400

    [publishing]: Fixup user playbook schema

--- db/playbooks.ts
+++ db/playbooks.ts
@@ -1,7 +1,7 @@
 import { Table } from '@/spec/sql'
 import { z } from 'zod'
 import { v4 as uuidv4 } from 'uuid'
-import { z_uuid, nullable_undefined_codec } from '@/utils/zod'
+import { z_uuid, nullable_undefined_codec, z_bigint_codec } from '@/utils/zod'
 
 export const user_playbook = Table.create('user_playbook')
   .field('id', 'uuid', 'default uuid_generate_v4()', z_uuid(), { primaryKey: true, default: uuidv4 })
@@ -12,7 +12,7 @@ export const user_playbook = Table.create('user_playbook')
   .field('inputs', 'varchar', '', nullable_undefined_codec(z.string()))
   .field('outputs', 'varchar', '', nullable_undefined_codec(z.string()))
   .field('bco', 'varchar', '', nullable_undefined_codec(z.string()))
-  .field('clicks', 'number', '', z.number(), { default: () => 0 })
-  .field('public', 'boolean not null', 'default false', z.boolean(), { default: () => false })
+  .field('clicks', 'bigint', 'not null default 0', z_bigint_codec(), { default: () => 0 })
+  .field('public', 'boolean', 'not null default false', z.boolean(), { default: () => false })
   .field('created', 'timestamp', 'not null default now()', z.date(), { default: () => new Date() })
   .build()

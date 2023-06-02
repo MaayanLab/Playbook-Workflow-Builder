673e9e10f45e79fe778de46c96358eb3ef0fc5d7
Daniel Clarke <u8sand@gmail.com>
2023-06-02 13:38:30 -0400

    Add mock for un-importable blueprintjs/icons

--- app/jest.config.js
+++ app/jest.config.js
@@ -11,6 +11,7 @@ const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/../$1',
+    '@blueprintjs/icons': '<rootDir>/tests/mock/@blueprintjs/icons/index.js',
   },
 }
 

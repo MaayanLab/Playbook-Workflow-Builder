17a173d196666daf8ef154613a3678eadebd697b
Pat McNeely <pmcneely2@gmail.com>
2023-06-06 13:47:34 -0400

    wip - working through Zod calling async functions

--- components/gly_gen/index.tsx
+++ components/gly_gen/index.tsx
@@ -5,18 +5,23 @@ import { z } from 'zod'
 import { glygen_icon } from '@/icons'
 import { GeneTerm } from '@/components/core/input/term'
 
-export const GlyGenResponse = z.object({
-  queryinfo: z.object({
-    query: z.object({
-      recommended_gene_name: z.string()
-    }),
-  }),
-  results: z.array(z.object({
-    uniprot_canonical_ac: z.string()
-  }))
+export const GlyGenIDtoResponse = z.object({
+  list_id: z.string(),
 })
 
+export const GlyGenResponse = z.object({ })
+//   queryinfo: z.object({
+//     query: z.object({
+//       gene_name: z.string()
+//     }),
+//   }),
+//   results: z.array(z.object({
+//     uniprot_canonical_ac: z.string()
+//   }))
+// })
+
 export type GlyGenResponseType = z.infer<typeof GlyGenResponse>
+export type GlyGenIDType = z.infer<typeof GlyGenIDtoResponse>
 
 export const GlyGenResponseNode = MetaNode('GlyGenResponse')
   .meta({
@@ -29,17 +34,55 @@ export const GlyGenResponseNode = MetaNode('GlyGenResponse')
     <div>
       Query:
       <pre >
-        {JSON.stringify(data.queryinfo.query, undefined, 2)}
+        {/* {JSON.stringify(data.queryinfo.query, undefined, 2)} */}
+        {JSON.stringify(data, undefined, 2)}
       </pre>
-      Results:
+      {/* Results:
       {data.results.map((result, index) => (
         <div key={index}>
           {JSON.stringify(result.uniprot_canonical_ac, undefined, 2)}
-        </div>
-      ))}
+        </div> */}
+      {/* ))} */}
     </div>
   ))
   .build()
+
+async function SearchGlyGenProteins(gene_name: string) {
+  // TODO: Breakout ID request here
+  const id_request = await fetch('https://api.glygen.org/directsearch/protein/search', {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ gene_name: gene_name }),
+    })
+  return id_request.json()
+}
+
+async function ResolveGlyGenProteinByID(gene_id: string){
+  // TODO: Breakout retrieval of info here
+  const protein_response = await fetch('https://api.glygen.org/directsearch/protein/list', {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id: gene_id }),
+    })
+  return protein_response
+}
+
+async function SearchAndResolve(gene_name: string) {
+  try {
+    const glygen_id = await SearchGlyGenProteins(gene_name);
+    const response_body = await ResolveGlyGenProteinByID(glygen_id['list_id']);
+    return response_body.json()
+  } catch(e) {
+    console.log(e)
+    throw e
+  }
+}
 
 export const ProteinProductInformation = MetaNode('ProteinProductInformation')
   .meta({
@@ -51,17 +94,30 @@ export const ProteinProductInformation = MetaNode('ProteinProductInformation')
   .inputs({ gene: GeneInfo })
   .output(GlyGenResponseNode)
   .resolve(async (props) => {
-    const query = encodeURIComponent(JSON.stringify({
-      recommended_gene_name: props.inputs.gene.symbol,
-    }))
-    const request = await fetch(`https://api.glygen.org/directsearch/protein/?query=${query}`, {
-      method: 'GET',
-      headers: {
-        accept: 'application/json'
-      }
-      })
-    const response = GlyGenResponse.parse(await request.json())
-    return response
+    // const query = encodeURIComponent(JSON.stringify({
+    //   gene_name: props.inputs.gene.symbol,
+    // }))
+    // const id_request = await fetch('https://api.glygen.org/directsearch/protein/search', {
+    //   method: 'POST',
+    //   headers: {
+    //     accept: 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ gene_name: props.inputs.gene.symbol }),
+    //   })
+    // const protein_response = await fetch('https://api.glygen.org/directsearch/protein/list', {
+    //   method: 'POST',
+    //   headers: {
+    //     accept: 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ id: GlyGenIDtoResponse.parse(await id_request.json())['list_id'] }),
+    //   })
+    await SearchAndResolve(props.inputs.gene.symbol).then(response => {
+      return response
+    })
+    // const response = GlyGenResponse.parse()
+    // return response
   })
   .story(props =>
     `Next, the GlyGen database [\\ref{doi:10.1093/glycob/cwz080}] was searched to identify a relevant set of proteins that originate from ${props.inputs.gene.symbol}.`

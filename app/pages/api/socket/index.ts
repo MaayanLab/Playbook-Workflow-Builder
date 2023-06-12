d0dae8840b11dc47cadf8021533f9a1385ed9ce6
Daniel Clarke <u8sand@gmail.com>
2023-06-12 17:49:19 -0400

    Add Socket.IO endpoint

---
+++ app/pages/api/socket/index.ts
@@ -0,0 +1,14 @@
+import { Server } from 'socket.io'
+
+export default function SocketHandler(req: any, res: any) {
+  if (res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+    io.on('connection', (client) => {
+    })
+  }
+  res.end()
+}

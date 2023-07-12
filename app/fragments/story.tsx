ddb012a61c190fcda5b2c4970a9a5555d874b8e8
Daniel Clarke <u8sand@gmail.com>
2023-07-12 16:32:06 -0400

    [story]: Don't pass errors to input/output of story

--- app/fragments/story.tsx
+++ app/fragments/story.tsx
@@ -9,8 +9,10 @@
 import React from 'react'
 import type KRG from '@/core/KRG'
 import type { Metapath } from '@/app/fragments/metapath'
+import type { MetaNode } from '@/spec/metanode'
 import { useMetapathInputs, useMetapathOutput } from '@/app/fragments/metapath'
 import extractCitations from '@/utils/citations'
+import * as dict from '@/utils/dict'
 
 /**
  * Attempt to compute the story for any given step
@@ -24,8 +26,8 @@ function StoryNode({ krg, head, onChange }: { krg: KRG, head: Metapath, onChange
     try {
       return <>
         {processNode.story({
-          inputs,
-          output,
+          inputs: inputs && dict.values(inputs).some(inp => (inp as MetaNode).spec === 'Error') ? undefined : inputs,
+          output: output && output.spec === 'Error' ? undefined : output,
         })}&nbsp;
       </>
     } catch (e) {

5252d7176f496337bcc12b974f55850dea8dae9d
Daniel Clarke <u8sand@gmail.com>
2023-07-20 16:12:47 -0400

    [gene_signature]: Tweak geneset construction to produce good sized sets

--- components/data/gene_signature/__init__.py
+++ components/data/gene_signature/__init__.py
@@ -64,10 +64,26 @@ def gene_signature(file: File) -> Signature:
     ellipses=ellipses,
   )
 
+def resonable_geneset_threshold(d, up_down=False):
+  ''' Maybe this should be manually specified by the user but for now we try several sane choices
+  and pick the one which results in a geneset >0 but closest to 250 up/down genes.
+  '''
+  candidates = [
+    (abs(n_up - 250) + abs(n_down - 250), col, thresh)
+    for col, thresh in [('Pval', 0.05),('Pval', 0.01),('Pval', 0.001),('AdjPval', 0.05),('AdjPval', 0.01),('AdjPval', 0.001)]
+    for n_up, n_down in ((((d['LogFC']>0) & (d[col]<thresh)).sum(), ((d['LogFC']<0) & (d[col]<thresh)).sum()),)
+    if ((n_up > 0 and n_down > 0) if up_down else (n_up+n_down)>0)
+  ]
+  if not candidates:
+    return ('Pval', 0.05)
+  _, col, thresh = min(candidates)
+  return col, thresh
+
 def gmt_from_sig(sig: Signature):
   d = signature_from_file(sig).sort_values('Pval', ascending=True)
-  up = d[(d['LogFC'] > 0) & (d['AdjPval'] < 0.05)].tolist()
-  down = d[(d['LogFC'] < 0) & (d['AdjPval'] < 0.05)].tolist()
+  col, thresh = resonable_geneset_threshold(d, up_down=True)
+  up = d[(d['LogFC'] > 0) & (d[col] < thresh)].index.tolist()
+  down = d[(d['LogFC'] < 0) & (d[col] < thresh)].index.tolist()
   return {
     f"{sig['description']} Up Genes": {
       'description': f"Significant up genes for the {sig['description']}",
@@ -81,10 +97,11 @@ def gmt_from_sig(sig: Signature):
 
 def geneset_from_sig(sig: Signature, direction):
   d = signature_from_file(sig).sort_values('Pval', ascending=True)
+  col, thresh = resonable_geneset_threshold(d, up_down=True)
   if direction == 'up':
-    top = d[(d['LogFC'] > 0) & (d['AdjPval'] < 0.05)].tolist()
+    top = d[(d['LogFC'] > 0) & (d[col] < thresh)].index.tolist()
   else:
-    top = d[(d['LogFC'] < 0) & (d['AdjPval'] < 0.05)].tolist()
+    top = d[(d['LogFC'] < 0) & (d[col] < thresh)].index.tolist()
   return {
     'description': f"Significant {direction} genes from the {sig['description']}",
     'set': top
@@ -99,8 +116,9 @@ def jsonifyable_float(x):
 def scored_genes_from_sig(sig: Signature):
   from scipy.stats import norm
   d = signature_from_file(sig)
-  d = d[d['AdjPval']<0.05]
-  zscores = pd.Series(np.sign(d['LogFC']) * norm.ppf(1-d['AdjPval']), index=d.index)
+  col, thresh = resonable_geneset_threshold(d)
+  d = d[d[col]<thresh]
+  zscores = pd.Series(np.sign(d['LogFC']) * norm.ppf(1-d[col]), index=d.index)
   return [
     dict(term=term, zscore=jsonifyable_float(zscore))
     for term, zscore in zscores.to_dict().items()

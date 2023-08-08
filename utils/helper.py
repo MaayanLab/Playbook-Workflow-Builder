81ca8ac769ded7bdc74d30cfa136795daae0ba84
Daniel Clarke <u8sand@gmail.com>
2023-08-08 14:42:21 -0400

    [python]: allow returning a stream

--- utils/helper.py
+++ utils/helper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Usage: ./python_helper.py mod.path.func_name <<< '{"kargs": ["value"]}' >>> '{"output": "json"}'
-import sys, json, pathlib, importlib, contextlib
+import sys, json, inspect, pathlib, importlib, contextlib
 _, pathspec = sys.argv
 name, _, attr = pathspec.rpartition('.')
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.absolute()))
@@ -9,4 +9,7 @@ with contextlib.redirect_stdout(sys.stderr):
   func = getattr(mod, attr)
   args = json.load(sys.stdin)
   out = func(*(args.get('kargs') or []), **(args.get('kwargs') or {}))
-json.dump(out, sys.stdout, allow_nan=False)
+if inspect.isgenerator(out):
+  sys.stdout.buffer.writelines(out)
+else:
+  json.dump(out, sys.stdout, allow_nan=False)

ea012185f0e85469b1a405749d7105994fd879c9
Daniel Clarke <u8sand@gmail.com>
2023-08-10 17:26:40 -0400

    [db]: sessions default to null (like pg)

--- db/sessions.ts
+++ db/sessions.ts
@@ -5,7 +5,7 @@ import { z_uuid } from '@/utils/zod'
 
 export const proxy_session = Table.create('proxy_session')
   .field('id', 'uuid', 'default uuid_generate_v4()', z_uuid(), { primaryKey: true, default: uuidv4 })
-  .field('state', 'varchar', '', z.string().nullable())
-  .field('run_id', 'varchar', '', z.string().nullable())
+  .field('state', 'varchar', '', z.string().nullable(), { default: () => null })
+  .field('run_id', 'varchar', '', z.string().nullable(), { default: () => null })
   .field('created', 'timestamp', 'not null default now()', z.date(), { default: () => new Date() })
   .build()

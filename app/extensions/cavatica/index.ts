e81a15bcd07ef0bcc09b4e85eeb768a295b431bf
Daniel Clarke <u8sand@gmail.com>
2023-08-11 17:07:31 -0400

    [cavatica]: Update ufs

--- app/extensions/cavatica/index.ts
+++ app/extensions/cavatica/index.ts
@@ -1,7 +1,7 @@
 import uuid5 from "@/utils/uuid"
 
 const docker_tag = 'maayanlab/playbook-partnership'
-const version = 'c12'
+const version = 'c13'
 const cwl = {
   "cwlVersion": "v1.2",
   "class": "CommandLineTool",

50b77e4566f5d59048a7e97f5ba4963d3cc7c0a9
Daniel Clarke <u8sand@gmail.com>
2023-08-11 15:30:59 -0400

    Update versions

--- app/extensions/cavatica/index.ts
+++ app/extensions/cavatica/index.ts
@@ -1,7 +1,7 @@
 import uuid5 from "@/utils/uuid"
 
 const docker_tag = 'maayanlab/playbook-partnership'
-const version = 'c10'
+const version = 'c11'
 const cwl = {
   "cwlVersion": "v1.2",
   "class": "CommandLineTool",

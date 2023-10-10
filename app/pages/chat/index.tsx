5f3e0f6ff326795947ed37559f3230b466f12bf1
Daniel Clarke <u8sand@gmail.com>
2023-10-10 17:22:56 -0400

    Make sure user is logged in to use chat

--- app/pages/chat/index.tsx
+++ app/pages/chat/index.tsx
@@ -2,37 +2,53 @@ import React from 'react'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useSessionWithId } from '@/app/extensions/next-auth/hooks'
 
 const Layout = dynamic(() => import('@/app/fragments/playbook/layout'))
 
 export default function Chat() {
+  const { data } = useSessionWithId()
   return (
     <Layout>
       <Head><title>Chat</title></Head>
       <main className="flex-grow container mx-auto p-4 flex flex-col gap-6 justify-center place-items-center">
         <div className="prose">
-          Chat Mode is still in development, we're currently testing out several methods for obtaining responses. The buttons below allow you to test these different methods.
+          <p>Chat Mode is still in development, we're currently testing out several methods for obtaining responses. The buttons below allow you to test these different methods.</p>
         </div>
-        <div className="flex flex-row  justify-center place-items-center gap-6">
+        {!data?.user?.id ?
+          <Link href="/api/auth/signin?redirect=/chat">
+            <button
+              className="btn btn-warning"
+            ><b>Please log in to use this feature</b></button>
+          </Link>
+          : null}
+        <div className="flex flex-row flex-wrap justify-center place-items-center gap-6">
           <Link href="/chat/gpt-prompting">
-          
             <div className="tooltip tooltip-bottom" data-tip="Works through prompts to OpenAI's completions endpoint.">
-              <button className="btn btn-xl btn-primary">GPT Few-shot Prompting</button>
+              <button className="btn btn-xl btn-primary" disabled={!data?.user?.id}>
+                GPT Few-shot Prompting
+              </button>
             </div>
           </Link>
           <Link href="/chat/gpt-embedding-clf">
             <div className="tooltip tooltip-bottom" data-tip="Works by prioritizing workflow steps based on your prompt's GPT Embedding.">
-              <button className="btn btn-xl btn-primary">GPT Embedding Classifier</button>
+              <button className="btn btn-xl btn-primary" disabled={!data?.user?.id}>
+                GPT Embedding Classifier
+              </button>
             </div>
           </Link>
           <Link href="/chat/transformer">
             <div className="tooltip tooltip-bottom" data-tip="Works by translating english text into a PWB workflow.">
-            <button className="btn btn-xl btn-primary">English -&gt; Workflow Transformer</button>
+            <button className="btn btn-xl btn-primary" disabled={!data?.user?.id}>
+              English -&gt; Workflow Transformer
+            </button>
             </div>
           </Link>
           <Link href="/chat/transformer-embed">
             <div className="tooltip tooltip-bottom" data-tip="Works by translating english text into a PWB workflow.">
-            <button className="btn btn-xl btn-primary">English + GPT Embedding -&gt; Workflow Transformer</button>
+            <button className="btn btn-xl btn-primary" disabled={!data?.user?.id}>
+              English + GPT Embedding -&gt; Workflow Transformer
+            </button>
             </div>
           </Link>
         </div>

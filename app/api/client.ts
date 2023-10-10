f4ad631aec3022d406924007ef16f45bb211bffe
Daniel Clarke <u8sand@gmail.com>
2023-10-10 09:25:45 -0400

    Add transformer

--- app/api/client.ts
+++ app/api/client.ts
@@ -8,6 +8,8 @@ import type { ChatGPTEmbeddingCLF as ChatGPTEmbeddingCLF_ } from './v1/chat'
 export const ChatGPTEmbeddingCLF = APIInterface<typeof ChatGPTEmbeddingCLF_>("/api/v1/chat/gpt-embedding-clf", "POST")
 import type { ChatGPTPrompting as ChatGPTPrompting_ } from './v1/chat'
 export const ChatGPTPrompting = APIInterface<typeof ChatGPTPrompting_>("/api/v1/chat/gpt-prompting", "POST")
+import type { ChatTransformer as ChatTransformer_ } from './v1/chat'
+export const ChatTransformer = APIInterface<typeof ChatTransformer_>("/api/v1/chat/transformer", "POST")
 import type { UserIntegrationsCAVATICA as UserIntegrationsCAVATICA_ } from './v1/user/integrations/cavatica'
 export const UserIntegrationsCAVATICA = APIInterface<typeof UserIntegrationsCAVATICA_>("/api/v1/user/integrations/cavatica", "GET")
 import type { UserIntegrationsCAVATICAUpdate as UserIntegrationsCAVATICAUpdate_ } from './v1/user/integrations/cavatica'

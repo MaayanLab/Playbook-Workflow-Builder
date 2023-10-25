226916f48d77a34163b0cc5c4db24c37fe0bcef3
Daniel Clarke <u8sand@gmail.com>
2023-10-25 12:19:45 -0400

    Fixup theme toggle, respect user browser settings

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -2,25 +2,41 @@ import React from 'react'
 import useLocalStorage from '@/utils/localstorage'
 
 export default function ThemeToggle() {
-  const [theme, setTheme] = useLocalStorage('light' as 'light' | 'dark')
+  const [theme, setTheme] = useLocalStorage('theme')
+  const [currentTheme, setCurrentTheme] = React.useState<string>(() => window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
   React.useEffect(() => {
-    if (theme === 'dark') {
+    if (!window.matchMedia) return
+    const media = window.matchMedia('(prefers-color-scheme: dark)')
+    const listener = (evt: MediaQueryListEvent) => {
+      setTheme(null)
+      setCurrentTheme(evt.matches ? 'dark' : 'light')
+    }
+    media.addEventListener('change', listener)
+    return () => {
+      media.removeEventListener('change', listener)
+    }
+  }, [])
+  React.useEffect(() => {
+    if (theme === null) return
+    setCurrentTheme(theme === 'dark' ? 'dark' : 'light')
+  }, [theme])
+  React.useEffect(() => {
+    if (currentTheme === 'dark') {
       document.getElementsByTagName('html')[0].setAttribute('data-theme', 'dark')
       document.getElementsByTagName('html')[0].classList.add('dark')
       document.getElementsByTagName('html')[0].classList.add('bp5-dark')
-      return () => {
-        document.getElementsByTagName('html')[0].setAttribute('data-theme', 'light')
-        document.getElementsByTagName('html')[0].classList.remove('dark')
-        document.getElementsByTagName('html')[0].classList.remove('bp5-dark')
-      }
+    } else {
+      document.getElementsByTagName('html')[0].setAttribute('data-theme', 'light')
+      document.getElementsByTagName('html')[0].classList.remove('dark')
+      document.getElementsByTagName('html')[0].classList.remove('bp5-dark')
     }
-  }, [theme])
+  }, [currentTheme])
   return (
     <input
       type="checkbox"
       className="toggle"
       title="Toggle light/dark theme"
-      checked={theme === 'dark'}
+      checked={currentTheme === 'dark'}
       onChange={evt => {setTheme(evt.currentTarget.checked ? 'dark' : 'light')}}
     />
   )

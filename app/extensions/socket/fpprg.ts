c0a3cdde2c85c27639fd1fde4a5e10677cbe434d
Daniel Clarke <u8sand@gmail.com>
2023-10-31 18:17:25 -0400

    Forward resolve_process notify to clients awaiting them

--- app/extensions/socket/fpprg.ts
+++ app/extensions/socket/fpprg.ts
@@ -2,6 +2,7 @@ import type { Socket } from 'socket.io'
 import fpprg from '@/app/fpprg'
 import { TimeoutError } from '@/spec/error'
 import { Resolved } from '@/core/FPPRG'
+import db from '@/app/db'
 
 export function onSocketFPPRG(client: Socket) {
   client.on('fpprg:fpl', async (id: string) => {
@@ -13,6 +14,11 @@ export function onSocketFPPRG(client: Socket) {
     }
   })
   client.on('fpprg:resolved', async (id: string) => {
+    // forward partial resolution update to the client
+    const unsub = db.listen(`distributed:resolved:${id}:status`, (status: string) => {
+      client.emit(`fpprg:resolved:${id}:status`, JSON.parse(status))
+    })
+    // wait for it to be resolved
     let resolved: Resolved | undefined
     while (resolved === undefined) {
       try {
@@ -21,11 +27,17 @@ export function onSocketFPPRG(client: Socket) {
         if (!(e instanceof TimeoutError)) {
           client.emit(`fpprg:resolved:${id}`, { error: 'An unexpected error occurred' } )
           console.error(e)
+          unsub()
           throw e
         }
       }
-      if (client.disconnected) return
+      if (client.disconnected) {
+        unsub()
+        return
+      }
     }
+    unsub()
+    // send the resolved instance to the requesting user
     client.emit(`fpprg:resolved:${resolved.id}`, resolved.toJSON())
   })
 }

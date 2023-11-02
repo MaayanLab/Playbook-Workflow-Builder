f3e5f767b486725e89ddb74efbb52cc37ee1209d
Daniel Clarke <u8sand@gmail.com>
2023-11-02 17:18:36 -0400

    Remove some logging

--- app/pages/api/socket/index.ts
+++ app/pages/api/socket/index.ts
@@ -8,10 +8,7 @@ export const config = {
 }
 
 export default function SocketHandler(req: any, res: any) {
-  if (res.socket.server.io) {
-    console.log('Socket is already running')
-  } else {
-    console.log('Socket is initializing')
+  if (!res.socket.server.io) {
     const io = new Server(res.socket.server)
     res.socket.server.io = io
     io.on('connection', onSocket)
--- utils/orm/pg.ts
+++ utils/orm/pg.ts
@@ -56,12 +56,10 @@ export class PgDatabase implements DbDatabase {
       this.listen('distributed:on_insert', async (rawPayload) => {
         const payload = db.notify_insertion_trigger_payload.codec.decode(rawPayload)
         const { table, operation, id } = payload
-        // console.debug(`received on_insert from ${table}: ${id}`)
         if (operation === 'INSERT') {
           this.notify(`insert:${table}`, { id })
         }
       })
-      // console.log('ready')
     })(this).catch((error) => {
       console.error('Failed to initialize subscriber', error)
       process.exit(1)

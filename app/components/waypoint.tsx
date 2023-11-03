70be84c579d75231c5e3f78844939e39ebceacfa
Daniel J. B. Clarke <u8sand@gmail.com>
2023-11-03 17:03:21 -0400

    Capture "active" scroll position in breadcrumbs

---
+++ app/components/waypoint.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Map } from 'immutable'
+
+const WaypointsContext = React.createContext({
+  waypoints: Map<string, { ref: HTMLDivElement, active?: boolean }>(),
+  set: (id: string, ref: HTMLDivElement) => {},
+  del: (id: string) => {},
+})
+
+export function useWaypoints() {
+  const { waypoints } = React.useContext(WaypointsContext)
+  return waypoints
+}
+
+export function Waypoints(props: React.PropsWithChildren<{ className?: string }>) {
+  const ref = React.useRef<HTMLDivElement>(null)
+  const [waypoints, setWaypoints] = React.useState(Map<string, { ref: HTMLDivElement, active?: boolean }>())
+  React.useEffect(() => {
+    if (!ref.current) return
+    const listener = () => {
+      if (!ref.current) return
+      const scrollTop = ref.current.scrollTop + window.innerHeight/4
+      const scrollBottom = scrollTop + window.innerHeight/2
+      setWaypoints(waypoints => waypoints.mapEntries(([key, value]) => {
+        const elTop = value.ref.offsetTop
+        const elBottom = value.ref.offsetTop + value.ref.clientHeight
+        const active = (scrollTop < elBottom && scrollBottom > elTop)
+        return [key, {
+          ref: value.ref,
+          active,
+        }]
+      }))
+    }
+    ref.current.addEventListener('scroll', listener)
+    return () => {
+      ref.current?.removeEventListener('scroll', listener)
+    }
+  }, [ref])
+  return (
+    <div ref={ref} className={props.className}>
+      <WaypointsContext.Provider value={{
+        waypoints,
+        set: (id: string, ref: HTMLDivElement) => {
+          setWaypoints(waypoints => waypoints.set(id, { ref }))
+        },
+        del: (id: string) => {
+          setWaypoints(waypoints => waypoints.delete(id))
+        },
+      }}>
+        {props.children}
+      </WaypointsContext.Provider>
+    </div>
+  )
+}
+
+export function Waypoint(props: React.PropsWithChildren<{ id: string }>) {
+  const ref = React.useRef<HTMLDivElement>(null)
+  const waypoints = React.useContext(WaypointsContext)
+  React.useEffect(() => {
+    if (ref.current === null) return
+    waypoints.set(props.id, ref.current)
+    return () => {waypoints.del(props.id)}
+  }, [props.id, ref])
+  return <div ref={ref}>{props.children}</div>
+}
--- app/fragments/playbook/layout.tsx
+++ app/fragments/playbook/layout.tsx
@@ -4,6 +4,7 @@ import type { Session } from 'next-auth'
 import { useRuntimeConfig } from '@/app/fragments/config'
 import usePublicUrl from '@/utils/next-public-url'
 import ThemeToggle from '@/app/components/ThemeToggle'
+import { Waypoints } from '@/app/components/waypoint'
 
 function UserAvatar({ session }: { session: Session | null }) {
   if (typeof session?.user?.image === 'string') {
@@ -27,7 +28,7 @@ export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <div className="drawer min-w-screen min-h-screen">
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col">
+      <Waypoints className="drawer-content flex flex-col">
         <div className="w-full navbar bg-primary gap-2">
           <div className="flex-grow flex-none">
             <div className="flex-none lg:hidden">
@@ -90,7 +91,7 @@ export default function Layout({ children }: React.PropsWithChildren) {
             </div>
           </div>
         </div>
-      </div>
+      </Waypoints>
       <div className="drawer-side">
         <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 bg-base-100">

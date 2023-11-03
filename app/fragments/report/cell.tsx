0f5ce49219062ee4bebd174187902a1e4be08af7
Daniel J. B. Clarke <u8sand@gmail.com>
2023-11-03 17:30:50 -0400

    Clicking breadcrumbs in report mode scrolls to waypoints

--- app/fragments/report/cell.tsx
+++ app/fragments/report/cell.tsx
@@ -7,6 +7,7 @@ import { Metapath, useMetapathOutput } from '@/app/fragments/metapath'
 import classNames from 'classnames'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 import { useStory } from '@/app/fragments/story'
+import { Waypoint } from '@/app/components/waypoint'
 
 const Prompt = dynamic(() => import('@/app/fragments/report/prompt'))
 const Icon = dynamic(() => import('@/app/components/icon'))
@@ -19,83 +20,87 @@ export default function Cell({ session_id, krg, id, head, cellMetadata, setCellM
   if (!processNode) return <div className="alert alert-error">Error: {head.process.type} does not exist</div>
   return (
     <>
-      {!('prompt' in processNode) ? <div className="flex-grow flex-shrink items-center overflow-auto bp5-card p-0">
-        <div className="collapse collapse-arrow text-black dark:text-white">
-          <input type="checkbox" checked={cellMetadata[head.id].process_visible} onChange={evt => {setCellMetadata((cellMetadata) => ({ ...cellMetadata, [head.id]: { ...cellMetadata[head.id], process_visible: evt.target.checked, id: '' } }))}} />
-          <div className="collapse-title flex flex-col gap-2">
-            <div className="flex flex-row gap-2">
-              <Icon icon={processNode.meta.icon || func_icon} className="fill-black dark:fill-white" />
-              <h2 className="bp5-heading">
-                {cellMetadata[head.id].label ? cellMetadata[head.id].label
-                  : processNode.meta.label ? processNode.meta.label
-                  : processNode.spec}
-              </h2>
+      <Waypoint id={`${head.id}:process`} style={{ scrollMarginTop: '6em' }}>
+        {!('prompt' in processNode) ? <div id={`${head.id}:process`} className="flex-grow flex-shrink items-center overflow-auto bp5-card p-0" style={{ scrollMarginTop: '6em' }}>
+          <div className="collapse collapse-arrow text-black dark:text-white">
+            <input type="checkbox" checked={cellMetadata[head.id].process_visible} onChange={evt => {setCellMetadata((cellMetadata) => ({ ...cellMetadata, [head.id]: { ...cellMetadata[head.id], process_visible: evt.target.checked, id: '' } }))}} />
+            <div className="collapse-title flex flex-col gap-2">
+              <div className="flex flex-row gap-2">
+                <Icon icon={processNode.meta.icon || func_icon} className="fill-black dark:fill-white" />
+                <h2 className="bp5-heading">
+                  {cellMetadata[head.id].label ? cellMetadata[head.id].label
+                    : processNode.meta.label ? processNode.meta.label
+                    : processNode.spec}
+                </h2>
+              </div>
+              <p className="prose max-w-none">{nodeStories[head.id]}</p>
             </div>
-            <p className="prose max-w-none">{nodeStories[head.id]}</p>
-          </div>
-          <div className="collapse-content">
-            <p className="bp5-ui-text">
-              <ReactMarkdown>
-                {cellMetadata[head.id].description ?? processNode.meta.description ?? ''}
-              </ReactMarkdown>
-            </p>
-          </div>
-        </div>
-        <div className={classNames('border-t-secondary border-t-2 mt-2', { 'hidden': !cellMetadata[head.id].process_visible })}>
-          <Link href={`${session_id ? `/session/${session_id}` : ''}/graph/${id}/node/${head.id}`}>
-            <button className="bp5-button bp5-minimal">
-              <Icon icon={view_in_graph_icon} />
-            </button>
-          </Link>
-        </div>
-      </div> : null}
-      <div className="flex-grow flex-shrink items-center overflow-auto bp5-card p-0">
-        {'prompt' in processNode ?
-          <Prompt
-            session_id={session_id}
-            id={id}
-            krg={krg}
-            head={head}
-            processNode={processNode}
-            outputNode={outputNode}
-            output={output}
-          />
-          : <div className="collapse collapse-arrow text-black dark:text-white">
-          <input type="checkbox" checked={cellMetadata[head.id].data_visible} onChange={evt => {setCellMetadata((cellMetadata) => ({ ...cellMetadata, [head.id]: { ...cellMetadata[head.id], data_visible: evt.target.checked, id: '' } }))}} />
-          <div className="collapse-title flex flex-col gap-2">
-            <div className="flex flex-row gap-2">
-              <Icon icon={(outputNode && outputNode.meta.icon) || variable_icon} className="fill-black dark:fill-white" />
-              <h2 className="bp5-heading">{(outputNode && (outputNode.meta.label || processNode.spec)) || "Loading"}</h2>
+            <div className="collapse-content">
+              <p className="bp5-ui-text">
+                <ReactMarkdown>
+                  {cellMetadata[head.id].description ?? processNode.meta.description ?? ''}
+                </ReactMarkdown>
+              </p>
             </div>
           </div>
-          <div className="collapse-content">
-            {outputNode && View && output ? View(output) : isLoading ? 'Waiting for results' : 'Waiting for input'}
+          <div className={classNames('border-t-secondary border-t-2 mt-2', { 'hidden': !cellMetadata[head.id].process_visible })}>
+            <Link href={`${session_id ? `/session/${session_id}` : ''}/graph/${id}/node/${head.id}`}>
+              <button className="bp5-button bp5-minimal">
+                <Icon icon={view_in_graph_icon} />
+              </button>
+            </Link>
           </div>
-        </div>}
-        <div className={classNames('border-t-secondary border-t-2 mt-2', { 'hidden': !('prompt' in processNode) && !cellMetadata[head.id].data_visible })}>
-          <Link href={`${session_id ? `/session/${session_id}` : ''}/graph/${id}/node/${head.id}`}>
-            <button className="bp5-button bp5-minimal">
-              <Icon icon={view_in_graph_icon} className="fill-black dark:fill-white" />
+        </div> : null}
+      </Waypoint>
+      <Waypoint id={`${head.id}:data`} style={{ scrollMarginTop: '6em' }}>
+        <div className="flex-grow flex-shrink items-center overflow-auto bp5-card p-0">
+          {'prompt' in processNode ?
+            <Prompt
+              session_id={session_id}
+              id={id}
+              krg={krg}
+              head={head}
+              processNode={processNode}
+              outputNode={outputNode}
+              output={output}
+            />
+            : <div className="collapse collapse-arrow text-black dark:text-white">
+            <input type="checkbox" checked={cellMetadata[head.id].data_visible} onChange={evt => {setCellMetadata((cellMetadata) => ({ ...cellMetadata, [head.id]: { ...cellMetadata[head.id], data_visible: evt.target.checked, id: '' } }))}} />
+            <div className="collapse-title flex flex-col gap-2">
+              <div className="flex flex-row gap-2">
+                <Icon icon={(outputNode && outputNode.meta.icon) || variable_icon} className="fill-black dark:fill-white" />
+                <h2 className="bp5-heading">{(outputNode && (outputNode.meta.label || processNode.spec)) || "Loading"}</h2>
+              </div>
+            </div>
+            <div className="collapse-content">
+              {outputNode && View && output ? View(output) : isLoading ? 'Waiting for results' : 'Waiting for input'}
+            </div>
+          </div>}
+          <div className={classNames('border-t-secondary border-t-2 mt-2', { 'hidden': !('prompt' in processNode) && !cellMetadata[head.id].data_visible })}>
+            <Link href={`${session_id ? `/session/${session_id}` : ''}/graph/${id}/node/${head.id}`}>
+              <button className="bp5-button bp5-minimal">
+                <Icon icon={view_in_graph_icon} className="fill-black dark:fill-white" />
+              </button>
+            </Link>
+            <Link href={`${session_id ? `/session/${session_id}` : ''}/graph/${id}/node/${head.id}/extend`}>
+              <button className="bp5-button bp5-minimal">
+                <Icon icon={fork_icon} className="fill-black dark:fill-white" />
+              </button>
+            </Link>
+            <button className="bp5-button bp5-minimal" disabled>
+              {isLoading ?
+                <Icon icon={status_waiting_icon} className="fill-yellow-500" />
+                : (outputNode ?
+                    (output ?
+                      (outputNode.spec === 'Error' ?
+                        <Icon icon={status_alert_icon} className="fill-red-500" />
+                        : <Icon icon={status_complete_icon} className="fill-green-500" />)
+                      : <Icon icon={status_awaiting_input_icon} className="fill-yellow-600" />)
+                    : <Icon icon={status_waiting_icon} className="fill-yellow-300" />)}
             </button>
-          </Link>
-          <Link href={`${session_id ? `/session/${session_id}` : ''}/graph/${id}/node/${head.id}/extend`}>
-            <button className="bp5-button bp5-minimal">
-              <Icon icon={fork_icon} className="fill-black dark:fill-white" />
-            </button>
-          </Link>
-          <button className="bp5-button bp5-minimal" disabled>
-            {isLoading ?
-              <Icon icon={status_waiting_icon} className="fill-yellow-500" />
-              : (outputNode ?
-                  (output ?
-                    (outputNode.spec === 'Error' ?
-                      <Icon icon={status_alert_icon} className="fill-red-500" />
-                      : <Icon icon={status_complete_icon} className="fill-green-500" />)
-                    : <Icon icon={status_awaiting_input_icon} className="fill-yellow-600" />)
-                  : <Icon icon={status_waiting_icon} className="fill-yellow-300" />)}
-          </button>
+          </div>
         </div>
-      </div>
+      </Waypoint>
     </>
   )
 }

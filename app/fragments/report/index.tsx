ffc7b2e1c25b2bf1c4a7ee3db8f18f41cd5fc4ca
Daniel Clarke <u8sand@gmail.com>
2023-11-10 17:14:22 -0500

    Don't break on undefined fallback serialization

--- app/fragments/report/index.tsx
+++ app/fragments/report/index.tsx
@@ -40,7 +40,7 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   }
   return {
     props: {
-      fallback,
+      fallback: JSON.parse(JSON.stringify(fallback)),
     }
   }
 }

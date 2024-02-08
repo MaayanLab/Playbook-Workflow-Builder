5d66a578a5a85a48973929cf165d8df7d580b8ff
Daniel Clarke <u8sand@gmail.com>
2024-02-08 17:13:38 -0500

    Configure socket.io postgres adapter

--- app/pages/api/socket/index.ts
+++ app/pages/api/socket/index.ts
@@ -1,12 +1,27 @@
+import db from '@/app/db'
 import emitter from '@/app/emitter'
 import { Server } from 'socket.io'
+import { createAdapter } from '@socket.io/postgres-adapter'
+import { PgDatabase } from '@/utils/orm/pg'
 
 export default function SocketHandler(req: any, res: any) {
   if (res.socket.server.io) {
     console.log('Socket is already running')
   } else {
     console.log('Socket is initializing')
-    const io = new Server(res.socket.server)
+    const io = new Server(res.socket.server, { transports: ['websocket'] })
+    if ('pool' in db) {
+      // if there would be multiple instances of the ui, this will ensure
+      //  messages can be shared across processes
+      (db as PgDatabase).pool.query(`
+        CREATE TABLE IF NOT EXISTS socket_io_attachments (
+          id          bigserial UNIQUE,
+          created_at  timestamptz DEFAULT NOW(),
+          payload     bytea
+        );
+      `)
+      io.adapter(createAdapter((db as PgDatabase).pool))
+    }
     res.socket.server.io = io
     io.on('connection', (client) => {
       console.debug(`${client.id} connected`)

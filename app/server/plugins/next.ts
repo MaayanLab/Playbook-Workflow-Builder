29e130f55585683474acec1278ff6e0110d1baca
Daniel Clarke <u8sand@gmail.com>
2024-02-15 11:25:02 -0500

    Get joint nextjs/socket.io dev/prod server working

--- app/server/plugins/next.ts
+++ app/server/plugins/next.ts
@@ -1,39 +1,25 @@
-import http from 'http';
 import { parse } from 'url'
+import http from 'http'
 import next from 'next'
 import path from 'path'
 import conf from '@/app/next.config'
 
-export default async function plugin(opts: { hostname: string, port: number }) {
+export default async function plugin(server: http.Server, opts: { hostname: string, port: number }) {
   console.log('Starting NextJS server...')
-  const dir = path.dirname(path.dirname(__dirname))
+  const dir = process.env.APP_ROOT ? `${process.env.APP_ROOT}/app` : path.dirname(path.dirname(__dirname))
   const dev = process.env.NODE_ENV !== 'production'
-  const app = next({ dev, hostname: opts.hostname, port: opts.port, dir, conf })
-  const handle = app.getRequestHandler()
-  await app.prepare()
-  const server = http.createServer(async (req, res) => {
+  const nextApp = next({ dev, hostname: opts.hostname, port: opts.port, dir, conf })
+  const nextHandler = nextApp.getRequestHandler()
+  await nextApp.prepare()
+  server.on('request', async (req, res) => {
     try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      if (!req.url) throw new Error('url is undefined')
+      if (!req.url) throw new Error('Unhandled Error')
       const parsedUrl = parse(req.url, true)
-      if (parsedUrl.pathname?.startsWith('/socket.io')) {
-        console.log('ignoreing socket.io')
-        return
-      }
-      await handle(req, res, parsedUrl)
+      await nextHandler(req, res, parsedUrl)
     } catch (err) {
       console.error('Error occurred handling', req.url, err)
       res.statusCode = 500
       res.end('internal server error')
     }
   })
-  server.once('error', (err) => {
-    console.error(err)
-    process.exit(1)
-  })
-  await new Promise<void>((resolve, reject) => {
-    server.listen(opts.port, () => {resolve()})
-  })
-  console.log(`Server listening on http://${opts.hostname}:${opts.port}`);
 }
--- app/server/plugins/ws.ts
+++ app/server/plugins/ws.ts
@@ -5,13 +5,8 @@ import { createAdapter } from '@socket.io/postgres-adapter'
 import { PgDatabase } from '@/utils/orm/pg'
 import onSocket from '@/app/extensions/socket'
 
-export default async function plugin(opts: { hostname: string, port: number }) {
-  console.log('Starting socket.io server...')
-  const server = http.createServer()
-  server.once('error', (err) => {
-    console.error(err)
-    process.exit(1)
-  })
+export default async function plugin(server: http.Server, opts: {}) {
+  console.log('Starting Socket.io server...')
   const io = new Server(server, { transports: ['websocket'] });
   if ('pool' in db) {
     // if there would be multiple instances of the ui, this will ensure
@@ -26,8 +21,4 @@ export default async function plugin(opts: { hostname: string, port: number }) {
     io.adapter(createAdapter((db as PgDatabase).pool))
   }
   io.on('connection', (socket) => onSocket(io, socket));
-  await new Promise<void>((resolve, reject) => {
-    server.listen(opts.port, () => {resolve()})
-  })
-  console.log(`Server listening on http://${opts.hostname}:${opts.port}`);
 }

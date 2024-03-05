fa2ec34b06cc67803133421fc524823cbccd6099
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-03-05 18:29:51 -0500

    add glycan set metanodes

--- components/gly_gen/glycan/index.tsx
+++ components/gly_gen/glycan/index.tsx
@@ -6,9 +6,11 @@ import { GlycanTerm } from '@/components/core/input/term'
 import { GlycanSet } from '@/components/core/input/set'
 import { GlycanResponse, GlyGenGlycanSetResponse } from "./data_models";
 import { GlycanClassification, GlycanCrossRef } from "./sup_components";
+import { glycan_set_search_query } from "./sup_functions";
 
 // --- Data Metanodes --- //
 
+// Single glycan data metanode 
 export const GlycanViewResponseNode = MetaNode('GlycanViewResponse')
   .meta({
     label: 'Glycan information',
@@ -42,8 +44,53 @@ export const GlycanViewResponseNode = MetaNode('GlycanViewResponse')
   })
   .build()
 
+// Glycan set data metanode
+export const GlyGenGlycanSetResponseNode = MetaNode('GlyGenGlycanSetResponse')
+  .meta({
+    label: 'GlyGen Glycans',
+    description: 'Protein product records in GlyGen',
+    icon: [glygen_icon],
+  })
+  .codec(GlyGenGlycanSetResponse)
+  .view( data => {
+    return (
+      <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Glycan ID</th>
+                <th>Glycan Image</th>
+                <th>Hit Score</th>
+                <th>Monoisotopic Mass</th>
+                <th>Monoisotopic Mass-pMe (Da)</th>
+                <th>No of Sugars</th>
+                <th>No of Glycoproteins</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((entry, index) => (
+                <tr key = {index}>
+                  <td>{entry.glytoucan.glytoucan_ac}</td>
+                  <td><img src={`https://api.glygen.org/glycan/image/${entry.glytoucan.glytoucan_ac}/`} alt='Glycan Image'/></td>
+                  <td>{entry.hit_score}</td>
+                  <td>{`${entry.mass}`}</td>
+                  <td>{entry.mass_pme}</td>
+                  <td>{entry.sugar_count}</td>
+                  <td>{entry.glycoprotein_count}</td>
+                  <td>{entry.associated_enzymes}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+      </div>
+    )
+  })
+  .build()
+
+
 // --- Process Metanodes --- //
 
+// Single glycan process metanode
 export const GlycanInformation = MetaNode('GlycanInformation')
   .meta({
     label: 'Search GlyGen by GlyTouCan Accession',
@@ -68,6 +115,27 @@ export const GlycanInformation = MetaNode('GlycanInformation')
   })
   .story(props => 
     `The GlyGen database [\\ref{doi:10.1093/glycob/cwz080}] was searched to identify a information about ${props.inputs ? props.inputs.glycan : 'the glycan'}.`
+  )
+  .build()
+
+// Glycan set process metanode
+export const GlyGenGlycanSet = MetaNode('GGGS')
+  .meta({
+    label: 'Search GlyGen by GlyTouCan Accession for Glycans',
+    description: 'Find glycan records in GlyGen.',
+    icon: [glygen_icon],
+    pagerank: 2,
+  })
+  .inputs({ glycan_glytoucan_acc_set: GlycanSet })
+  .output(GlyGenGlycanSetResponseNode)
+  .resolve(async (props) => {
+    console.log("===> Got glycan set input: ", props.inputs.glycan_glytoucan_acc_set.set);
+    const glycan_response = await glycan_set_search_query(props.inputs.glycan_glytoucan_acc_set.set);
+    return glycan_response;
+  })
+  .story(props =>
+    // TODO: re-write story sentence to make sense with protein term input (previous gene value removed to prevent `npm run build` error)
+    `Next, the GlyGen database [\\ref{doi:10.1093/glycob/cwz080}] was searched to identify a relevant set of glycans that originate from.`
   )
   .build()
 

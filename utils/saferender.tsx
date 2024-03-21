330dfdd33155b7787765b46f61e3c79c893e7765
Daniel J. B. Clarke <u8sand@gmail.com>
2024-03-21 15:30:01 -0400

    Adopt react-error-boundary for SafeRender

--- utils/saferender.tsx
+++ utils/saferender.tsx
@@ -1,7 +1,11 @@
-export default function SafeRender<T>({ component: Component, props }: { component: (props: T) => React.ReactNode, props: T }) {
-  try {
-    return Component(props)
-  } catch (e: any) {
-    return <div className="alert alert-error">{e.toString()}</div>
-  }
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+
+function Fallback({ error }: FallbackProps) {
+  return <div className="alert alert-error">{error.message}</div>
+}
+
+export default function SafeRender<T>({ component: Component, props }: { component: (props: T) => React.ReactNode, props: T & JSX.IntrinsicAttributes }) {
+  return <ErrorBoundary FallbackComponent={Fallback}>
+    <Component {...props} />
+  </ErrorBoundary>
 }

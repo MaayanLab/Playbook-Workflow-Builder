16e57b8d7d72db78daa3bcf9a392e642ce39ecaf
Miroslav Milinkov <miroslav.milinkov@persida-bio.com>
2024-04-16 02:31:47 +0200

    chnages requested from keyang, CTD coloring of the different nodes, error handling in case of unresponsive API's

--- components/service/ctd/service.tsx
+++ components/service/ctd/service.tsx
@@ -340,15 +340,30 @@ export const CTD_Graph_Nodes = MetaNode('CTD_Graph_Nodes')
   .inputs({ ctdResponseInfo: CTDResponseInfo })
   .output(GraphPlot)
   .resolve(async (props) => {
+    let highlyConnectedGenes = props.inputs.ctdResponseInfo.highlyConnectedGenes;
     let jsonGraph = props.inputs.ctdResponseInfo.jsonGraph;
 
     if(jsonGraph == null){
       throw new Error("No Gene Graph Nodes available, please use a different input gene set!");
     }
 
+    let graphNodes = jsonGraph.nodes;
+
+    if(graphNodes.length > 0){
+      for(let i in highlyConnectedGenes){
+        let hcGene = highlyConnectedGenes[i];
+        for(let k in graphNodes){
+          if(hcGene == graphNodes[k].name){
+            graphNodes[k].type = "hc";
+            break;
+          }
+        }
+      }
+    }
+
     return {
       edges: props.inputs.ctdResponseInfo.jsonGraph.interactions || [],
-      nodes: props.inputs.ctdResponseInfo.jsonGraph.nodes.map(({ name, ...rest }) => ({ ...rest, id: name || '(no name)' })),
+      nodes: graphNodes.map(({ name, ...rest }) => ({ ...rest, id: name || '(no name)' })),
     }
   }).story(props =>
     `Graph Nodes were extracted from the CTD output.`

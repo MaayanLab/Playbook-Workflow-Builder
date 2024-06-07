530d1650a5604d190c5b5fa511aaa31a201e51c9
Daniel Clarke <u8sand@gmail.com>
2024-06-07 13:36:23 -0400

    Disable report mode icon before a report is present

--- app/fragments/graph/report-button.tsx
+++ app/fragments/graph/report-button.tsx
@@ -6,7 +6,7 @@ const Icon = dynamic(() => import('@/app/components/icon'))
 
 export default function ReportButton({ session_id, graph_id, thread_id }: { session_id?: string, graph_id: string, thread_id?: string }) {
   const router = useRouter()
-  const disabled = router.asPath.endsWith('/graph') || router.asPath.endsWith('/graph/start') || router.asPath.endsWith('/graph/extend') || router.asPath.endsWith('/graph/start/extend')
+  const disabled = router.asPath.endsWith('/graph') || router.asPath.endsWith('/graph/start') || router.asPath.endsWith('/graph/extend') || router.asPath.endsWith('/graph/start/extend') || graph_id === 'start'
   return (
     <Link href={`${session_id ? `/session/${session_id}` : ''}/report${graph_id === 'start' ? `/` : `/${graph_id}`}${thread_id ? `?thread=${thread_id}` : ''}`}>
       <button className='bp5-button bp5-minimal' disabled={disabled}>

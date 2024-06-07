e9d8b5b50b8b3d704fcd6c06fd6a6512c8e533c5
Daniel Clarke <u8sand@gmail.com>
2024-06-07 12:48:40 -0400

    Improve styling and loading indicator

--- app/fragments/chat/chat.tsx
+++ app/fragments/chat/chat.tsx
@@ -30,7 +30,7 @@ export default function Page({ thread_id, session_id, embedded = false }: { thre
   const publicUrl = usePublicUrl()
   const [message, setMessage] = React.useState('')
   const [collapse, setCollapse] = React.useState(false)
-  const { data: session } = Auth.useSession()
+  const { data: session } = Auth.useSession({ required: true })
   const { data: { messages, fpl } = { messages: undefined, fpl: null }, mutate } = useAPIQuery(GPTAssistantMessagesList, { thread_id })
   const { trigger, isMutating } = useAPIMutation(GPTAssistantMessage, { thread_id })
   // const { trigger: triggerDelete } = useAPIMutation(GPTAssistantDelete, { thread_id })
@@ -55,6 +55,31 @@ export default function Page({ thread_id, session_id, embedded = false }: { thre
   const { waypoints, scrollTo } = useWaypoints()
   return (
     <>
+      <style jsx>{`
+        /* From the newer version of DaisyUI */
+        .loading {
+          pointer-events: none;
+          display: inline-block;
+          aspect-ratio: 1/1;
+          width: 1.5rem;
+          background-color: currentColor;
+          -webkit-mask-size: 100%;
+          mask-size: 100%;
+          -webkit-mask-repeat: no-repeat;
+          mask-repeat: no-repeat;
+          -webkit-mask-position: center;
+          mask-position: center;
+          -webkit-mask-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0nMjQnIGhlaWdodD0nMjQnIHN0cm9rZT0nIzAwMCcgdmlld0JveD0nMCAwIDI0IDI0JyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnPjxzdHlsZT4uc3Bpbm5lcl9WOG0xe3RyYW5zZm9ybS1vcmlnaW46Y2VudGVyO2FuaW1hdGlvbjpzcGlubmVyX3pLb2EgMnMgbGluZWFyIGluZmluaXRlfS5zcGlubmVyX1Y4bTEgY2lyY2xle3N0cm9rZS1saW5lY2FwOnJvdW5kO2FuaW1hdGlvbjpzcGlubmVyX1lwWlMgMS41cyBlYXNlLW91dCBpbmZpbml0ZX1Aa2V5ZnJhbWVzIHNwaW5uZXJfektvYXsxMDAle3RyYW5zZm9ybTpyb3RhdGUoMzYwZGVnKX19QGtleWZyYW1lcyBzcGlubmVyX1lwWlN7MCV7c3Ryb2tlLWRhc2hhcnJheTowIDE1MDtzdHJva2UtZGFzaG9mZnNldDowfTQ3LjUle3N0cm9rZS1kYXNoYXJyYXk6NDIgMTUwO3N0cm9rZS1kYXNob2Zmc2V0Oi0xNn05NSUsMTAwJXtzdHJva2UtZGFzaGFycmF5OjQyIDE1MDtzdHJva2UtZGFzaG9mZnNldDotNTl9fTwvc3R5bGU+PGcgY2xhc3M9J3NwaW5uZXJfVjhtMSc+PGNpcmNsZSBjeD0nMTInIGN5PScxMicgcj0nOS41JyBmaWxsPSdub25lJyBzdHJva2Utd2lkdGg9JzMnPjwvY2lyY2xlPjwvZz48L3N2Zz4=);
+          mask-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0nMjQnIGhlaWdodD0nMjQnIHN0cm9rZT0nIzAwMCcgdmlld0JveD0nMCAwIDI0IDI0JyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnPjxzdHlsZT4uc3Bpbm5lcl9WOG0xe3RyYW5zZm9ybS1vcmlnaW46Y2VudGVyO2FuaW1hdGlvbjpzcGlubmVyX3pLb2EgMnMgbGluZWFyIGluZmluaXRlfS5zcGlubmVyX1Y4bTEgY2lyY2xle3N0cm9rZS1saW5lY2FwOnJvdW5kO2FuaW1hdGlvbjpzcGlubmVyX1lwWlMgMS41cyBlYXNlLW91dCBpbmZpbml0ZX1Aa2V5ZnJhbWVzIHNwaW5uZXJfektvYXsxMDAle3RyYW5zZm9ybTpyb3RhdGUoMzYwZGVnKX19QGtleWZyYW1lcyBzcGlubmVyX1lwWlN7MCV7c3Ryb2tlLWRhc2hhcnJheTowIDE1MDtzdHJva2UtZGFzaG9mZnNldDowfTQ3LjUle3N0cm9rZS1kYXNoYXJyYXk6NDIgMTUwO3N0cm9rZS1kYXNob2Zmc2V0Oi0xNn05NSUsMTAwJXtzdHJva2UtZGFzaGFycmF5OjQyIDE1MDtzdHJva2UtZGFzaG9mZnNldDotNTl9fTwvc3R5bGU+PGcgY2xhc3M9J3NwaW5uZXJfVjhtMSc+PGNpcmNsZSBjeD0nMTInIGN5PScxMicgcj0nOS41JyBmaWxsPSdub25lJyBzdHJva2Utd2lkdGg9JzMnPjwvY2lyY2xlPjwvZz48L3N2Zz4=)
+        }
+        .loading-dots {
+          -webkit-mask-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0nMjQnIGhlaWdodD0nMjQnIHZpZXdCb3g9JzAgMCAyNCAyNCcgeG1sbnM9J2h0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnJz48c3R5bGU+LnNwaW5uZXJfcU04M3thbmltYXRpb246c3Bpbm5lcl84SFFHIDEuMDVzIGluZmluaXRlfS5zcGlubmVyX29YUHJ7YW5pbWF0aW9uLWRlbGF5Oi4xc30uc3Bpbm5lcl9aVExme2FuaW1hdGlvbi1kZWxheTouMnN9QGtleWZyYW1lcyBzcGlubmVyXzhIUUd7MCUsNTcuMTQle2FuaW1hdGlvbi10aW1pbmctZnVuY3Rpb246Y3ViaWMtYmV6aWVyKDAuMzMsLjY2LC42NiwxKTt0cmFuc2Zvcm06dHJhbnNsYXRlKDApfTI4LjU3JXthbmltYXRpb24tdGltaW5nLWZ1bmN0aW9uOmN1YmljLWJlemllcigwLjMzLDAsLjY2LC4zMyk7dHJhbnNmb3JtOnRyYW5zbGF0ZVkoLTZweCl9MTAwJXt0cmFuc2Zvcm06dHJhbnNsYXRlKDApfX08L3N0eWxlPjxjaXJjbGUgY2xhc3M9J3NwaW5uZXJfcU04MycgY3g9JzQnIGN5PScxMicgcj0nMycvPjxjaXJjbGUgY2xhc3M9J3NwaW5uZXJfcU04MyBzcGlubmVyX29YUHInIGN4PScxMicgY3k9JzEyJyByPSczJy8+PGNpcmNsZSBjbGFzcz0nc3Bpbm5lcl9xTTgzIHNwaW5uZXJfWlRMZicgY3g9JzIwJyBjeT0nMTInIHI9JzMnLz48L3N2Zz4=);
+          mask-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0nMjQnIGhlaWdodD0nMjQnIHZpZXdCb3g9JzAgMCAyNCAyNCcgeG1sbnM9J2h0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnJz48c3R5bGU+LnNwaW5uZXJfcU04M3thbmltYXRpb246c3Bpbm5lcl84SFFHIDEuMDVzIGluZmluaXRlfS5zcGlubmVyX29YUHJ7YW5pbWF0aW9uLWRlbGF5Oi4xc30uc3Bpbm5lcl9aVExme2FuaW1hdGlvbi1kZWxheTouMnN9QGtleWZyYW1lcyBzcGlubmVyXzhIUUd7MCUsNTcuMTQle2FuaW1hdGlvbi10aW1pbmctZnVuY3Rpb246Y3ViaWMtYmV6aWVyKDAuMzMsLjY2LC42NiwxKTt0cmFuc2Zvcm06dHJhbnNsYXRlKDApfTI4LjU3JXthbmltYXRpb24tdGltaW5nLWZ1bmN0aW9uOmN1YmljLWJlemllcigwLjMzLDAsLjY2LC4zMyk7dHJhbnNmb3JtOnRyYW5zbGF0ZVkoLTZweCl9MTAwJXt0cmFuc2Zvcm06dHJhbnNsYXRlKDApfX08L3N0eWxlPjxjaXJjbGUgY2xhc3M9J3NwaW5uZXJfcU04MycgY3g9JzQnIGN5PScxMicgcj0nMycvPjxjaXJjbGUgY2xhc3M9J3NwaW5uZXJfcU04MyBzcGlubmVyX29YUHInIGN4PScxMicgY3k9JzEyJyByPSczJy8+PGNpcmNsZSBjbGFzcz0nc3Bpbm5lcl9xTTgzIHNwaW5uZXJfWlRMZicgY3g9JzIwJyBjeT0nMTInIHI9JzMnLz48L3N2Zz4=)
+        }
+        .loading-lg {
+          width: 2.5rem;
+        }
+      `}</style>
       <SessionStatus session_id={session_id}>
         <StoryProvider krg={krg} metapath={metapath ?? []}>
           {!embedded && metapath ?
@@ -213,7 +238,7 @@ export default function Page({ thread_id, session_id, embedded = false }: { thre
                 })}
                 {isMutating ?
                   <Message role="assistant" session={session}>
-                    <progress className="progress w-full"></progress>
+                    <span className="loading loading-dots loading-lg mt-2"></span>
                   </Message>
                   : null}
                 <Message role="user" session={session}>
--- app/pages/chat/index.tsx
+++ app/pages/chat/index.tsx
@@ -7,10 +7,13 @@ import * as Auth from 'next-auth/react'
 import classNames from 'classnames'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
+import usePublicUrl from '@/utils/next-public-url'
+import Message from '@/app/fragments/chat/message'
 const Layout = dynamic(() => import('@/app/fragments/playbook/layout'))
 
 export default function ChatThread() {
   const router = useRouter()
+  const publicUrl = usePublicUrl()
   const auth = useSessionWithId()
   const {trigger, error, isMutating} = useAPIMutation(GPTAssistantCreate)
   React.useEffect(() => {
@@ -20,26 +23,25 @@ export default function ChatThread() {
   return (
     <Layout>
       <Head><title>Chat</title></Head>
-      <main className="flex-grow container mx-auto py-4 flex flex-col">
-        <div className="prose">
-          <h2>Text to Workflow</h2>
-          <p>This is an experimental chat interface that translates your instructions to PWB workflows. Click start to begin!</p>
-          {!auth.data?.user?.id ? (
-            <div className="alert alert-warning shadow-lg block max-w-none">
-              You are required to &nbsp; <button className="btn btn-sm" onClick={() => {Auth.signIn()}}>sign in</button> &nbsp; to use the chat.
-            </div>
-          ) : (
-            <>
-              <div className="flex flex-row items-center">
-                <div className="btn btn-lg btn-primary" onClick={async (evt) => {
-                  const thread_id = await trigger()
-                  router.push(`/chat/${thread_id}`)
-                }}>Start</div>
+      <main className="flex-grow container mx-auto p-4 flex flex-col gap-6">
+        <div className={classNames("flex-grow flex flex-col justify-center items-center")}>
+          <img
+            className="w-32"
+            src={`${publicUrl}/PWB-logo.svg`}
+          />
+          <div className="prose"><h4>Playbook Workflow Builder Text to Workflow</h4></div>
+          <div className="flex flex-col overflow-hidden self-stretch">
+            <Message role="welcome" session={null}>
+              How can I help you today?
+            </Message>
+            <progress className={classNames('progress progress-primary', { 'hidden': !isMutating })} />
+            {error ? <div className="alert alert-error">{error.toString()}</div> : null}
+            <Message role="user" session={null}>
+              <div className="alert alert-warning shadow-lg block">
+                You are required to &nbsp; <button className="btn btn-sm" onClick={() => {Auth.signIn()}}>sign in</button> &nbsp; to use the chat.
               </div>
-              {error ? <div className="alert alert-error">{error.toString()}</div> : null}
-              <progress className={classNames('progress progress-primary', { 'hidden': !isMutating })} />
-            </>
-          )}
+            </Message>
+          </div>
         </div>
       </main>
     </Layout>

fcb02d41cb3a01c8f9bda1490e8cf13aeababe25
Daniel Clarke <u8sand@gmail.com>
2024-06-18 17:03:32 -0400

    Fix typo in workshop announcement

--- app/fragments/playbook/layout.tsx
+++ app/fragments/playbook/layout.tsx
@@ -58,9 +58,9 @@ export default function Layout({ children }: React.PropsWithChildren) {
         </div>
 
         <div className="container mx-auto my-2 flex flex-col gap-2">
-          <DismissableAlert id="workshop-2024-06-25-v2" className="alert-info">
+          <DismissableAlert id="workshop-2024-06-25-v3" className="alert-info">
             <Link href="/events/2024-06-25" className="dark:text-inherit dark:hover:text-inherit">
-              Come join our 1st open virtual workshop on Thursday, June 25, 2024! Click here for more information.
+              Come join our 1st open virtual workshop on Tuesday, June 25, 2024! Click here for more information.
             </Link>
           </DismissableAlert>
           <DismissableAlert id="biorxv" style={{ backgroundColor: '#d4fb79' }}>

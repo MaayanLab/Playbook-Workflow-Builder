fc8b1b67d3cac3b2caf8e76bc193d113438d3159
Daniel Clarke <u8sand@gmail.com>
2024-07-24 15:19:57 -0400

    Add a way to download the .tex version of the workflow

--- app/fragments/report/export-button.tsx
+++ app/fragments/report/export-button.tsx
@@ -46,6 +46,14 @@ export default function ExportButton({ session_id, id, metadata }: { session_id?
               download={`cwl-${id}.zip`}
             />
           </div>
+          <div className="tooltip block text-left" data-tip="Download a latex rendering of the playbook for creating a paper. This feature is in early ALPHA and is currently incomplete.">
+            <Bp5MenuItem
+              icon="document"
+              text="LaTeX Paper (ALPHA)"
+              href={`${session_id ? `/api/socket/${session_id}` : ''}/api/tex/${id}?metadata=${encodeURIComponent(JSON.stringify(metadata))}`}
+              download={`${id}.tex`}
+            />
+          </div>
           </Bp5Menu>
       }
       placement="bottom"

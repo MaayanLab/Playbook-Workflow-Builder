dcddd43e6fdd0e257003cd4cde94a0b374fbfa28
Daniel J. B. Clarke <u8sand@gmail.com>
2025-03-19 11:52:40 -0400

    File extension matching and .gmt.gz/.dmt.gz support

--- components/data/drug_matrix_transpose/__init__.py
+++ components/data/drug_matrix_transpose/__init__.py
@@ -1,11 +1,22 @@
 from components.core.file import file_as_stream
 
+def read_dmt(fr):
+  return {
+    term: dict(description=description, set=drugs)
+    for line in fr
+    for line_split in (line.decode().strip().split('\t'),)
+    if len(line_split) >= 3
+    for term, description, *drugs in (line_split,)
+  }
+
 def load_drug_matrix_transpose(file):
-  with file_as_stream(file) as fr:
-    return {
-      term: dict(description=description, set=drugs)
-      for line in fr
-      for line_split in (line.decode().strip().split('\t'),)
-      if len(line_split) >= 3
-      for term, description, *drugs in (line_split,)
-    }
+  if file['filename'].endswith('.dmt') or file['filename'].endswith('.gmt') or file['filename'].endswith('.txt'):
+    with file_as_stream(file, 'r') as fr:
+      return read_dmt(fr)
+  elif file['filename'].endswith('.dmt.gz') or file['filename'].endswith('.gmt.gz') or file['filename'].endswith('.txt.gz'):
+    import gzip
+    with file_as_stream(file, 'rb') as fr_compressed:
+      with gzip.open(fr_compressed, 'rt') as fr:
+        return read_dmt(fr)
+  else:
+    raise NotImplementedError
--- components/data/gene_matrix_transpose/__init__.py
+++ components/data/gene_matrix_transpose/__init__.py
@@ -1,11 +1,22 @@
 from components.core.file import file_as_stream
 
+def read_gmt(fr):
+  return {
+    term: dict(description=description, set=genes)
+    for line in fr
+    for line_split in (line.strip().split('\t'),)
+    if len(line_split) >= 3
+    for term, description, *genes in (line_split,)
+  }
+
 def load_gene_matrix_transpose(file):
-  with file_as_stream(file, 'r') as fr:
-    return {
-      term: dict(description=description, set=genes)
-      for line in fr
-      for line_split in (line.strip().split('\t'),)
-      if len(line_split) >= 3
-      for term, description, *genes in (line_split,)
-    }
+  if file['filename'].endswith('.gmt') or file['filename'].endswith('.txt'):
+    with file_as_stream(file, 'r') as fr:
+      return read_gmt(fr)
+  elif file['filename'].endswith('.gmt.gz') or file['filename'].endswith('.txt.gz'):
+    import gzip
+    with file_as_stream(file, 'rb') as fr_compressed:
+      with gzip.open(fr_compressed, 'rt') as fr:
+        return read_gmt(fr)
+  else:
+    raise NotImplementedError

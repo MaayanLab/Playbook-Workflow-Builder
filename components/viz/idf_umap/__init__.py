b38dc60c284ac48e26a3beed4755b4310bb423c6
Daniel J. B. Clarke <u8sand@gmail.com>
2025-03-21 16:52:40 -0400

    Add leiden clustering to GMT UMAPs

--- components/viz/idf_umap/__init__.py
+++ components/viz/idf_umap/__init__.py
@@ -1,7 +1,7 @@
 import json
 import typing
 import pandas as pd
-from umap import UMAP
+import scanpy as sc
 import plotly.express as px
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -12,11 +12,21 @@ class DescriptionSet(typing.TypedDict):
 def idf_umap(x: dict[str, DescriptionSet]):
   terms, sets = zip(*x.items())
   matrix = TfidfVectorizer(analyzer=lambda s: s['set']).fit_transform(sets)
-  umap = pd.DataFrame(UMAP(random_state=42).fit_transform(matrix), columns=['UMAP-1', 'UMAP-2'], index=terms)
+  df = sc.AnnData(matrix)
+  sc.pp.neighbors(df, use_rep='X')
+  sc.tl.umap(df, n_components=2, random_state=42)
+  sc.tl.leiden(df, key_added="leiden")
+
+  umap = pd.DataFrame({
+    'Leiden':df.obs['leiden'].values,
+    'UMAP-1':df.obsm['X_umap'][:,0],
+    'UMAP-2':df.obsm['X_umap'][:,1],
+  }, index=terms).sort_values('Leiden')
   fig = px.scatter(
     umap,
     x='UMAP-1',
     y='UMAP-2',
+    color='Leiden',
     hover_data=[umap.index],
     height=1000,
   )

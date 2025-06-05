dff56b41c0e2a883afb29ef13bdfb4adf7e877f8
Daniel J. B. Clarke <u8sand@gmail.com>
2025-06-05 12:17:45 -0400

    [targetranger]: Upper genes & use t as zscore

--- components/service/targetranger/__init__.py
+++ components/service/targetranger/__init__.py
@@ -20,7 +20,7 @@ def targetscreener(m: t.Union[AnnDataFile, GeneCountMatrix], *, bg):
   mean = d.X.mean(axis=0)
   std = d.X.std(axis=0)
   genes = pd.DataFrame({
-    gene: { 'mean': mean[i], 'std': std[i] }
+    gene.upper(): { 'mean': mean[i], 'std': std[i] }
     for i, gene in enumerate(d.var.index)
   })
   # submit to targetranger API
@@ -35,10 +35,9 @@ def targetscreener(m: t.Union[AnnDataFile, GeneCountMatrix], *, bg):
   res = req.json()
   if len(res) == 0: raise Exception(f"No targets found")
   res = pd.DataFrame(res).dropna()
+  res = res.rename({'t': 'zscore', 'gene': 'term'}, axis=1)
   # Translate response into output
-  res = res.sort_values('adj_p', ascending=True)
-  res['zscore'] = st.norm.ppf(1-res['adj_p'])
-  res.rename({ 'gene': 'term' }, axis=1, inplace=True)
+  res = res.sort_values('zscore', ascending=False)
   res['zscore'].replace(np.nan, 'nan', inplace=True)
   res['zscore'].replace(np.inf, 'inf', inplace=True)
   res['zscore'].replace(-np.inf, '-inf', inplace=True)

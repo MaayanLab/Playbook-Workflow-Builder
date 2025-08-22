3cd5d834623b6f7dbec17df4ddc95c2ca5e33c59
Daniel Clarke <u8sand@gmail.com>
2025-08-22 13:14:00 -0400

    Fixup deepdive

--- components/service/deepdive/index.tsx
+++ components/service/deepdive/index.tsx
@@ -16,7 +16,7 @@ export const GeneDeepDiveSummary = MetaNode(`GeneDeepDiveSummary`)
     <div className="flex-grow flex flex-row m-0" style={{ minHeight: 800 }}>
       <IFrame
         className="flex-grow border-0"
-        src={`${deepdive_url}/gene/${props}/50`}
+        src={`${deepdive_url}/${props}/`}
       />
     </div>
   ))
@@ -31,7 +31,7 @@ export const GeneDeepDive = MetaNode(`GeneDeepDive`)
   .inputs({ gene: GeneTerm })
   .output(GeneDeepDiveSummary)
   .resolve(async (props) => {
-    return props.inputs.gene.toUpperCase()
+    return props.inputs.gene
   })
   .story(props => ({
     abstract: `The top 50 most cited paper abstracts that mention ${props.inputs?.gene ?? 'the gene'} were summarized using DeepDive\\ref{DeepDive, https://maayanlab.cloud/deepdive/}.`,

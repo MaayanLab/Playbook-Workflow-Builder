ffdced92cb7909ac4dd3aaf7057095bedc942614
Daniel Clarke <u8sand@gmail.com>
2026-05-11 18:05:43 -0400

    Fixup db decoding errors in production

--- db/chat.ts
+++ db/chat.ts
@@ -1,21 +1,21 @@
 import { Table } from '@/spec/sql'
 import { z } from 'zod'
 import { v4 as uuidv4 } from 'uuid'
-import { json_safe_timestamp_codec, z_uuid } from '@/utils/zod'
+import { json_safe_timestamp_codec, nullable_undefined_codec, z_uuid } from '@/utils/zod'
 
 export const thread = Table.create('thread')
   .field('id', 'uuid', 'default uuid_generate_v4()', z_uuid(), { primaryKey: true, default: uuidv4 })
   .field('user', 'uuid', 'references "user" ("id") on delete cascade', z_uuid())
-  .field('openai_thread', 'varchar', '', z.string().nullable(), { default: () => null })
+  .field('openai_thread', 'varchar', '', nullable_undefined_codec(z.string()), { default: () => null })
   .field('created', 'timestamp without time zone', 'not null default now()', json_safe_timestamp_codec(), { default: () => new Date() })
   .build()
 
 export const thread_message = Table.create('thread_message')
   .field('id', 'uuid', 'default uuid_generate_v4()', z_uuid(), { primaryKey: true, default: uuidv4 })
-  .field('fpl', 'uuid', 'references "fpl" ("id") on delete cascade', z_uuid().nullable())
+  .field('fpl', 'uuid', 'references "fpl" ("id") on delete cascade', nullable_undefined_codec(z_uuid()), { default: () => null })
   .field('thread', 'uuid', 'not null references "thread" ("id") on delete cascade', z_uuid())
   .field('role', 'varchar', 'not null', z.string())
   .field('content', 'varchar', 'not null', z.string())
-  .field('feedback', 'varchar', '', z.string().nullable())
+  .field('feedback', 'varchar', '', nullable_undefined_codec(z.string()), { default: () => null })
   .field('created', 'timestamp without time zone', 'not null default now()', json_safe_timestamp_codec(), { default: () => new Date() })
   .build()

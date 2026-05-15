81bfb41bd07a317cc3cc8c385027ec89163fef25
Daniel J. B. Clarke <u8sand@gmail.com>
2026-05-15 11:35:30 -0400

    Setup pgvector extension

--- db/fpl-embedding.ts
+++ db/fpl-embedding.ts
@@ -1,6 +1,11 @@
-import { Table } from '@/spec/sql'
+import { SQL, Table } from '@/spec/sql'
 import { z } from 'zod'
 import { z_uuid } from '@/utils/zod'
+
+export const pg_vector = SQL.create()
+  .up(`create extension if not exists "vector";`)
+  .down(`drop extension "vector";`)
+  .build()
 
 export const fpl_embedding = Table.create('fpl_embedding')
   .field('id', 'uuid', 'primary key references "fpl" ("id") on delete cascade', z_uuid())

f08b736525562ce4749bb69792670251a09032d2
Daniel J. B. Clarke <u8sand@gmail.com>
2026-05-15 17:03:53 -0400

    Add a helper to embed text content

--- core/semantic-search/index.ts
+++ core/semantic-search/index.ts
@@ -8,6 +8,15 @@ import OpenAI from "openai"
 export async function indexSoon(props: { db: typeof DB, fpl_id: string }) {
     await props.db.send('embed', { id: props.fpl_id })
 }
+export async function embed(props: { openai: OpenAI, content: string }) {
+    const embeddings = await props.openai.embeddings.create({
+        input: props.content,
+        model: 'text-embedding-3-small',
+        dimensions: 1536,
+    })
+    return embeddings.data[0].embedding
+}
+
 export async function index(props: { openai: OpenAI, db: typeof DB, fpprg: FPPRG, krg: KRG, fpl_id: string }) {
     const fpl = await props.fpprg.getFPL(props.fpl_id)
     if (!fpl) throw new NotFoundError()
@@ -15,16 +24,12 @@ export async function index(props: { openai: OpenAI, db: typeof DB, fpprg: FPPRG
     if (fpl_embedding) return fpl_embedding.embedding
     const { story } = await fpl_expand({ krg: props.krg, fpl })
     const input = story.ast.flatMap(part => part.tags.includes('abstract') ? [part.type === 'bibitem' ? '\n' : '', part.text] : []).join('')
-    const embeddings = await props.openai.embeddings.create({
-        input,
-        model: 'text-embedding-3-small',
-        dimensions: 1536,
-    })
+    const embedding = await embed({ openai: props.openai, content: input })
     await props.db.objects.fpl_embedding.create({
         data: {
             id: props.fpl_id,
-            embedding: embeddings.data[0].embedding,
+            embedding,
         }
     })
-    return embeddings.data[0].embedding
+    return embedding
 }
